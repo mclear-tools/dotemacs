e4d78a6dc162a2272bdadfa6506e54fb2940ec89
Roambot <mclear@fastmail.com>
2016-11-15 20:36:42 -0600

    Fix ignore and typos

--- custom.el
+++ custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
  '(battery-update-interval 10)
  '(bmkp-last-as-first-bookmark-file "~/.emacs.d/.cache/bookmarks")
  '(counsel-locate-cmd (quote counsel-locate-cmd-mdfind))
@@ -14,11 +16,12 @@
     (tags-table-mode dired-mode Info-mode info-lookup-mode fundamental-mode)))
  '(fancy-battery-mode t)
  '(fancy-battery-show-percentage t)
- '(markdown-command "pandoc" t)
- '(markdown-enable-math t t)
- '(markdown-footnote-location (quote immediately) t)
- '(markdown-nested-imenu-heading-index t t)
- '(markdown-open-command "/Users/Roambot/bin/scripts/mark.sh" t)
+ '(hl-sexp-background-color "#1c1f26")
+ '(markdown-command "pandoc")
+ '(markdown-enable-math t)
+ '(markdown-footnote-location (quote immediately))
+ '(markdown-nested-imenu-heading-index t)
+ '(markdown-open-command "/Users/Roambot/bin/scripts/mark.sh")
  '(org-footnote-auto-adjust t)
  '(org-footnote-section "Footnotes")
  '(org-structure-template-alist
@@ -67,7 +70,7 @@
  '(org-zotxt-link-description-style :betterbibtexkey)
  '(package-selected-packages
    (quote
-    (page-break-lines dashboard darkroom writeroom-mode persistent-scratch creamsody-theme shackle paper-theme paganini-theme org-beautify-theme monokai-theme monochrome-theme minimal-theme lush-theme flatland-theme darktooth-theme darkokai-theme dakrone-theme clues-theme apropospriate-theme atom-dark-theme badwolf-theme badwolf zotxt auctex coffee-mode preview apples-mode lispy company yasnippet git-gutter+ magit pdf-tools evil helm projectile avy bookmark+ helm-projectile helm-files helm-dired-recent-dirs helm-flyspell helm-swoop helm-hunks helm-ls-git helm-descbinds helm-ag yaml-mode wttrin workgroups2 window-numbering which-key web-mode visual-regexp virtualenvwrapper vimrc-mode use-package toc-org sr-speedbar spaceline solarized-theme smooth-scrolling shell-switcher shell-pop sane-term reveal-in-osx-finder restart-emacs ranger rainbow-mode rainbow-identifiers rainbow-delimiters persp-projectile paradox pandoc-mode palimpsest ox-reveal ox-pandoc org-plus-contrib org-pdfview org-bullets multi-term material-theme markdown-mode lorem-ipsum lispyville interleave info+ htmlize hl-todo highlight-numbers helm-themes helm-bibtex haskell-mode gruvbox-theme git-timemachine git-gutter-fringe+ general fancy-battery evil-surround evil-numbers evil-magit evil-indent-textobject evil-escape evil-commentary elisp-slime-nav deft dash-at-point crux counsel-projectile company-math company-auctex centered-window-mode centered-cursor-mode bind-map back-button ag)))
+    (php-mode page-break-lines dashboard darkroom writeroom-mode persistent-scratch creamsody-theme shackle paper-theme paganini-theme org-beautify-theme monokai-theme monochrome-theme minimal-theme lush-theme flatland-theme darktooth-theme darkokai-theme dakrone-theme clues-theme apropospriate-theme atom-dark-theme badwolf-theme badwolf zotxt auctex coffee-mode preview apples-mode lispy company yasnippet git-gutter+ magit pdf-tools evil helm projectile avy bookmark+ helm-projectile helm-files helm-dired-recent-dirs helm-flyspell helm-swoop helm-hunks helm-ls-git helm-descbinds helm-ag yaml-mode wttrin workgroups2 window-numbering which-key web-mode visual-regexp virtualenvwrapper vimrc-mode use-package toc-org sr-speedbar spaceline solarized-theme smooth-scrolling shell-switcher shell-pop sane-term reveal-in-osx-finder restart-emacs ranger rainbow-mode rainbow-identifiers rainbow-delimiters persp-projectile paradox pandoc-mode palimpsest ox-reveal ox-pandoc org-plus-contrib org-pdfview org-bullets multi-term material-theme markdown-mode lorem-ipsum lispyville interleave info+ htmlize hl-todo highlight-numbers helm-themes helm-bibtex haskell-mode gruvbox-theme git-timemachine git-gutter-fringe+ general fancy-battery evil-surround evil-numbers evil-magit evil-indent-textobject evil-escape evil-commentary elisp-slime-nav deft dash-at-point crux counsel-projectile company-math company-auctex centered-window-mode centered-cursor-mode bind-map back-button ag)))
  '(paradox-github-token t)
  '(safe-local-variable-values
    (quote
@@ -92,5 +95,5 @@
  '(helm-selection ((t (:foreground "#f7f438" :background "#64b5ea" :underline nil :weight bold))))
  '(linum-highlight-face ((t (:inherit default :foreground "goldenrod1"))))
  '(markdown-comment-face ((t (:weight bold :strike-through nil))))
- '(org-block ((t (:foreground "#2E8B57" :slant italic))))
+ '(org-block ((t (:foreground "#2E8B57"))))
  '(org-block-begin-line ((t (:foreground "#74a8a4" :weight bold :slant normal)))))
