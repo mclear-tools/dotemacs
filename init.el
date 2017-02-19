b23d0615159f618b1cd6e5f744e6888d92c8e940
Colin McLear <mclear@fastmail.com>
2017-02-19 15:11:47 -0600

    Add new init config for tangling org file

--- init.el
+++ init.el
@@ -62,22 +62,85 @@
   (require 'bind-key) ;; if you use any :bind variant
   (require 'diminish) ;; if you use diminish
 
+  ;; This is GPLv2. If you still don't know the details, read
+  ;; http://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
+  ;; From Holger Schurig's config.
+  (defun my-tangle-section-canceled ()
+    "Return t if the current section header was DISABLED, else nil."
+    (save-excursion
+      (if (re-search-backward "^\\*+\\s-+\\(.*?\\)?\\s-*$" nil t)
+          (string-prefix-p "DISABLED" (match-string 1))
+        nil)))
+
+  ;; This uses partially derived code from ob-core.el. So this snippet
+  ;; is GPLv3 or later. If you still don't know the details, read
+  ;; http://www.gnu.org/licenses/
+
+  (defun my-tangle-config-org (orgfile elfile)
+    "This function will write all source blocks from =config.org= into
+  =config.el= that are ...
+
+  - not marked as :tangle no
+  - have a source-code of =emacs-lisp=
+  - doesn't have the todo-marker DISABLED"
+    (let* ((body-list ())
+           (gc-cons-threshold most-positive-fixnum)
+           (org-babel-src-block-regexp   (concat
+                                          ;; (1) indentation                 (2) lang
+                                          "^\\([ \t]*\\)#\\+begin_src[ \t]+\\([^ \f\t\n\r\v]+\\)[ \t]*"
+                                          ;; (3) switches
+                                          "\\([^\":\n]*\"[^\"\n*]*\"[^\":\n]*\\|[^\":\n]*\\)"
+                                          ;; (4) header arguments
+                                          "\\([^\n]*\\)\n"
+                                          ;; (5) body
+                                          "\\([^\000]*?\n\\)??[ \t]*#\\+end_src")))
+      (with-temp-buffer
+        (insert-file-contents orgfile)
+        (goto-char (point-min))
+        (while (re-search-forward org-babel-src-block-regexp nil t)
+          (let ((lang (match-string 2))
+                (args (match-string 4))
+                (body (match-string 5))
+                (canc (my-tangle-section-canceled)))
+            (when (and (string= lang "emacs-lisp")
+                       (not (string-match-p ":tangle\\s-+no" args))
+                       (not canc))
+              (add-to-list 'body-list body)))))
+      (with-temp-file elfile
+        (insert (format ";; Don't edit this file, edit %s instead ...\n\n" orgfile))
+        (apply 'insert (reverse body-list)))
+      (message "Wrote %s ..." elfile)))
+
+  (let ((orgfile (locate-user-emacs-file "config.org"))
+      (elfile (locate-user-emacs-file "config.el")))
+  (when (or (not (file-exists-p elfile))
+            (file-newer-than-file-p orgfile elfile))
+    (my-tangle-config-org orgfile elfile))
+  (load-file elfile))
+
+  (defun my-tangle-config-org-hook-func ()
+    (when (string= "config.org" (buffer-name))
+  	(let ((orgfile (locate-user-emacs-file "config.org"))
+  		  (elfile (locate-user-emacs-file "config.el")))
+  	  (my-tangle-config-org orgfile elfile))))
+  (add-hook 'after-save-hook #'my-tangle-config-org-hook-func)
+
   ;; load config -- Note that config file must exist & have source code for tangling, otherwise errors get thrown!!
-  (defvar init-source-org-file (expand-file-name "config.org" user-emacs-directory)
-    "The file that our emacs initialization comes from")
+  ;; (defvar init-source-org-file (expand-file-name "config.org" user-emacs-directory)
+  ;;   "The file that our emacs initialization comes from")
 
-  (defvar init-source-el-file (expand-file-name "config.el" user-emacs-directory)
-    "The file that our emacs initialization is generated into")
+  ;; (defvar init-source-el-file (expand-file-name "config.el" user-emacs-directory)
+  ;;   "The file that our emacs initialization is generated into")
 
-  (if (file-exists-p init-source-org-file)
-      (if (and (file-exists-p init-source-el-file)
-               (time-less-p (nth 5 (file-attributes init-source-org-file)) (nth 5 (file-attributes init-source-el-file))))
-          (load-file init-source-el-file)
-        (if (fboundp 'org-babel-load-file)  
-            (org-babel-load-file init-source-org-file)
-          (message "Function not found: org-babel-load-file")
-          (load-file init-source-el-file)))
-    (error "Init org file '%s' missing." init-source-org-file))
+  ;; (if (file-exists-p init-source-org-file)
+  ;;     (if (and (file-exists-p init-source-el-file)
+  ;;              (time-less-p (nth 5 (file-attributes init-source-org-file)) (nth 5 (file-attributes init-source-el-file))))
+  ;;         (load-file init-source-el-file)
+  ;;       (if (fboundp 'org-babel-load-file)  
+  ;;           (org-babel-load-file init-source-org-file)
+  ;;         (message "Function not found: org-babel-load-file")
+  ;;         (load-file init-source-el-file)))
+  ;;   (error "Init org file '%s' missing." init-source-org-file))
 
   ;; Garbage collector - decrease threshold back to 5 MB
   (run-with-idle-timer
