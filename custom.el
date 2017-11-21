47c31255b882a8880ff4a6f26de3eb32b89b379a
Colin McLear <mclear@fastmail.com>
2017-11-21 00:36:37 -0600

    Safe themes

--- custom.el
+++ custom.el
@@ -4,6 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(bmkp-last-as-first-bookmark-file "/Users/roambot/.emacs.d/.local/@roambot/cache/bookmarks")
+ '(custom-safe-themes
+   (quote
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
  '(safe-local-variable-values (quote ((org-download-heading-lvl))))
  '(writeroom-restore-window-config t))
 (custom-set-faces
