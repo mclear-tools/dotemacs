87832d7b7ff489b9d9e6b14b2ddcc5497ce05a1c
Colin McLear <mclear@fastmail.com>
2017-11-20 23:18:16 -0600

    Change bookmark

--- custom.el
+++ custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(bmkp-last-as-first-bookmark-file "/Users/roambot/.emacs.d/.local/@roambot/cache/bookmarks" t)
+ '(bmkp-last-as-first-bookmark-file "/Users/roambot/.emacs.d/.local/@roambot/cache/bookmarks")
  '(safe-local-variable-values (quote ((org-download-heading-lvl))))
  '(writeroom-restore-window-config t))
 (custom-set-faces
