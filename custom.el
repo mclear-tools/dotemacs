af3b55783a89de85f5d1757a83d29b9f3b2c4309
Colin McLear <mclear@fastmail.com>
2017-11-20 19:13:00 -0600

    Major refactor
    - revise init
    - use straight.el as package manager
    - revise config

---
+++ custom.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(bmkp-last-as-first-bookmark-file "/Users/roambot/.emacs.d/.local/@roambot/cache/bookmarks" t)
+ '(safe-local-variable-values (quote ((org-download-heading-lvl))))
+ '(writeroom-restore-window-config t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-tooltip-common ((t (:inherit company-tooltip :weight bold :underline nil))))
+ '(company-tooltip-common-selection ((t (:inherit company-tooltip-selection :weight bold :underline nil))))
+ '(nlinum-current-line ((t (:inherit default :foreground "goldenrod1")))))
--- init.el
+++ init.el
@@ -7,10 +7,30 @@
       initial-scratch-message nil
       mode-line-format nil)
 
+;; Use straight.el for package management
+(let ((bootstrap-file (concat user-emacs-directory "straight/bootstrap.el"))
+      (bootstrap-version 2))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
+;; integrate straight.el with use package 
+(straight-use-package 'use-package)
+(setq use-package-always-ensure t)
+(setq load-prefer-newer t)
+
+;; install org for bootstrapping config
+(use-package org :defer t :ensure t)
+
+;; load the config file
 (let ((file-name-handler-alist nil))
-  (if (file-exists-p "~/.emacs.d/config.elc")
-      (load-file "~/.emacs.d/config.elc")
-    (progn
-      (require 'org)
-      (org-babel-load-file "~/.emacs.d/config.org"))))
+  (if (file-exists-p (expand-file-name "config.elc" user-emacs-directory))
+      (load-file (expand-file-name "config.elc" user-emacs-directory))
+    (require 'org)
+    (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))))
+
