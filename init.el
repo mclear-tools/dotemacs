208a459705943f0510d2710c960f5968f5ac01bb
Colin McLear <mclear@fastmail.com>
2017-12-24 00:54:14 -0500

    Update init file

--- init.el
+++ init.el
@@ -25,33 +25,13 @@
 
 ;; integrate straight.el with use package
 (straight-use-package 'use-package)
-(require 'use-package)
+(require 'use-package)  ;; use to get general working properly
 (setq straight-use-package-by-default t)
 (setq load-prefer-newer t)
 
 ;; install org for bootstrapping config
 (use-package org
-  :straight org-plus-contrib
-  :defer t
-  :init
-  ;; fix annoying warnings about version
-  (defun radian--org-git-version ()
-    "Return the abbreviated SHA for the Org Git repo."
-    (let ((default-directory (concat user-emacs-directory
-                                     "straight/repos/org/")))
-      (if (executable-find "git")
-          (with-temp-buffer
-            (call-process "git" nil '(t nil) nil
-                          "rev-parse" "--short" "HEAD")
-            (if (> (buffer-size) 0)
-                (string-trim (buffer-string))
-              "revision unknown"))
-        "git not available")))
-  (defalias #'org-git-version #'radian--org-git-version)
-  (defun org-release () "9.1.3") ; FIXME: replace with a real function
-  (provide 'org-version)
-  (with-eval-after-load 'org
-    (defalias #'org-git-version #'radian--org-git-version)))
+  :straight org-plus-contrib)
 
 ;; load the config file ;; The following tangles a new .el and tangles on save as well.
 ;; From novoid's config; see https://github.com/novoid/dot-emacs/blob/master/init.el
@@ -128,13 +108,13 @@ Note the weekly scope of the command's precision.")
   (load-file elfile))
 
 ;; when config.org is saved, re-generate config.el:
-;; (defun my-tangle-config-org-hook-func ()
-;;   (when (string= "config.org" (buffer-name))
-;; 	(let ((orgfile (concat user-emacs-directory "config.org"))
-;; 		  (elfile (concat user-emacs-directory "config.el")))
-;; 	  (my-tangle-config-org))))
-;; (add-hook 'after-save-hook 'my-tangle-config-org-hook-func)))
+(defun my-tangle-config-org-hook-func ()
+  (when (string= "config.org" (buffer-name))
+	(let ((orgfile (concat user-emacs-directory "config.org"))
+		  (elfile (concat user-emacs-directory "config.el")))
+	  (my-tangle-config-org))))
+(add-hook 'after-save-hook 'my-tangle-config-org-hook-func)
 
-;; (message "→★ loading init.el in %.2fs" (float-time (time-subtract (current-time) my-init-el-start-time))))
+(message "→★ loading init.el in %.2fs" (float-time (time-subtract (current-time) my-init-el-start-time))))
   ;; (straight-finalize-transaction))
-)
+
