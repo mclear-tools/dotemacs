cd219aee2c24fd64f6c1d292c8038ca5bf20aa0d
Colin McLear <mclear@fastmail.com>
2018-04-03 23:45:26 -0500

    Swap remember for persistent-scratch

--- .local/elpa/persistent-scratch-20170110.546/persistent-scratch-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "persistent-scratch" "20170110.546" "Preserve the scratch buffer across Emacs sessions" '((emacs "24")) :commit "551c655fa349e6f48e4e29f427fff7594f76ac1d" :url "https://github.com/Fanael/persistent-scratch")
