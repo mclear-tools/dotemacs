4d7c2827d1b7d33eb98a2de124fae80d1885d252
Colin McLear <mclear@fastmail.com>
2018-04-06 23:27:19 -0500

    Add applescript mode

---
+++ .local/elpa/applescript-mode-20090320.2332/applescript-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "applescript-mode" "20090320.2332" "major mode for editing AppleScript source" 'nil :commit "8f888cd80af1e0902b5609143facd3051bc94892" :keywords '("languages" "tools"))
