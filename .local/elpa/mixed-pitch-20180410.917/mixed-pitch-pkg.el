5e5a42acc8e17694670051e111ca55e4a78e6d48
Colin McLear <mclear@fastmail.com>
2018-05-04 15:24:38 -0400

    Try out mixed-pitch package

---
+++ .local/elpa/mixed-pitch-20180410.917/mixed-pitch-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "mixed-pitch" "20180410.917" "Use a variable pitch, keeping fixed pitch where it's sensible" '((emacs "24.3")) :commit "b6b1601c7a3eb9ab23e33192bc479bccc4dd5e7b" :url "https://github.com/jabranham/mixed-pitch")
