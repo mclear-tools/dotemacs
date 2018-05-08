c715ca397e5b9b8797cb6b828cd3bc28dcd80be6
Colin McLear <mclear@fastmail.com>
2018-05-08 00:09:02 -0400

    Update packages

---
+++ .local/elpa/alect-themes-20180504.1020/alect-themes-pkg.el
@@ -0,0 +1,8 @@
+(define-package "alect-themes" "20180504.1020" "Configurable light, dark and black themes for Emacs 24 or later"
+  '((emacs "24.0"))
+  :keywords
+  '("color" "theme")
+  :url "https://github.com/alezost/alect-themes")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ .local/elpa/ivy-20180506.1303/ivy-pkg.el
@@ -0,0 +1,8 @@
+(define-package "ivy" "20180506.1303" "Incremental Vertical completYon"
+  '((emacs "24.1"))
+  :keywords
+  '("matching")
+  :url "https://github.com/abo-abo/swiper")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
