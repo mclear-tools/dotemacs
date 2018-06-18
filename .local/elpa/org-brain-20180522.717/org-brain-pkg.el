9bc7ba29a20c2dcbf54850b6a3f435e224341ffa
Colin McLear <mclear@fastmail.com>
2018-06-17 23:32:16 -0400

    Try org-brain

---
+++ .local/elpa/org-brain-20180522.717/org-brain-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "org-brain" "20180522.717" "Org-mode concept mapping" '((emacs "25") (org "9")) :commit "3faf9303af3f2356e3444e69c22dc6c5774047d1" :keywords '("outlines" "hypermedia") :url "http://github.com/Kungsgeten/org-brain")
