80bca9f66c6dbb697bf4482e062cc27d5663ccd4
Colin McLear <mclear@fastmail.com>
2018-07-04 15:45:53 -0400

    Add dired+ & fix dired settings

---
+++ .local/elpa/dired+-20180326.218/dired+-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dired+" "20180326.218" "Extensions to Dired." 'nil :commit "b9d7d09e37cef5ab778def5c55ac8989cc4d0a4a" :keywords '("unix" "mouse" "directories" "diredp" "dired") :authors '(("Drew Adams")) :maintainer '(nil . "Drew Adams (concat \"drew.adams\" \"@\" \"oracle\" \".com\")") :url "https://www.emacswiki.org/emacs/download/dired%2b.el")
