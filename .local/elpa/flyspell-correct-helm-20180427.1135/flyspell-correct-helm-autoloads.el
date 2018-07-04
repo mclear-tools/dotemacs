ccc0fb902da687f2bcf8fcdb9f8943cf114a4aaf
Colin McLear <mclear@fastmail.com>
2018-07-04 15:14:12 -0400

    Try and fix spelling issues

---
+++ .local/elpa/flyspell-correct-helm-20180427.1135/flyspell-correct-helm-autoloads.el
@@ -0,0 +1,23 @@
+;;; flyspell-correct-helm-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "flyspell-correct-helm" "flyspell-correct-helm.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from flyspell-correct-helm.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "flyspell-correct-helm" '("flyspell-correct-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; flyspell-correct-helm-autoloads.el ends here
---
+++ .local/elpa/flyspell-correct-helm-20180427.1135/flyspell-correct-helm-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flyspell-correct-helm" "20180427.1135" "correcting words with flyspell via helm interface" '((flyspell-correct "0.4.0") (helm "1.9.0")) :commit "0486912f57ac2ec70c472b776c63360462cb32d7" :url "https://github.com/d12frosted/flyspell-correct")
