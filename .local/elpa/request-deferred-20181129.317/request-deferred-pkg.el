f3554c0a31716c7ba6d49921f3a0655d2c864fb6
Colin McLear <mclear@fastmail.com>
2018-12-05 00:09:01 -0600

    Add all updated packeges to elpa dir

---
+++ .local/elpa/request-deferred-20181129.317/request-deferred-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "request-deferred" "20181129.317" "Wrap request.el by deferred" '((deferred "0.3.1") (request "0.2.0")) :stars '(#("272" 0 3 (font-lock-face paradox-star-face)) #("37K" 0 3 (value 37005 font-lock-face paradox-download-face))) :commit "b12f54af8ab6656aa8c1143c5ccc7675c5bb02cd" :authors '(("Takafumi Arakaki <aka.tkf at gmail.com>")) :maintainer '("Takafumi Arakaki <aka.tkf at gmail.com>") :url "https://github.com/tkf/emacs-request")
