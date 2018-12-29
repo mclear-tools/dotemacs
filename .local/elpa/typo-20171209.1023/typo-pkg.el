4eb88bfea555923fa8eb147ecf63508e48e31d92
Colin McLear <mclear@fastmail.com>
2018-12-28 21:16:33 -0500

    Add typo mode

---
+++ .local/elpa/typo-20171209.1023/typo-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "typo" "20171209.1023" "Minor mode for typographic editing" 'nil :commit "9dad93b6f367f02f52c8d9bf15d446d922cec294" :keywords '("convenience" "wp") :authors '(("Jorgen Schaefer" . "forcer@forcix.cx")) :maintainer '("Jorgen Schaefer" . "forcer@forcix.cx") :url "https://github.com/jorgenschaefer/typoel")
