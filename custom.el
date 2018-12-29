102dd1a237f5e18a4348ee0ba7e449c65dc99e93
Colin McLear <mclear@fastmail.com>
2018-12-28 21:19:36 -0500

    Update custom bookmarks

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(beacon-mode t)
- '(bmkp-last-as-first-bookmark-file "~/.emacs.d/bookmarks")
+ '(bmkp-last-as-first-bookmark-file "~/.emacs.d/.local/@roambot.local/cache/bookmarks")
  '(column-number-mode t)
  '(custom-safe-themes
    (quote
