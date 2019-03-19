0a357a4d82c38eed71944ea1d0f17725525be42d
Colin McLear <mclear@fastmail.com>
2019-03-19 13:06:00 -0400

    Update packages

--- .local/elpa/zenburn-theme-20190126.1204/zenburn-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "zenburn-theme" "20190126.1204" "A low contrast color theme for Emacs." 'nil :stars '(#("632" 0 3 (font-lock-face paradox-star-face)) #("203K" 0 4 (value 203756 font-lock-face paradox-download-face))) :commit "66a3d09983e2905544c4dd9824ec3d658a4d7f67" :authors '(("Bozhidar Batsov" . "bozhidar@batsov.com")) :maintainer '("Bozhidar Batsov" . "bozhidar@batsov.com") :url "http://github.com/bbatsov/zenburn-emacs")
