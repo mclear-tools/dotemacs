7398706867973516914a02369c984096b14d6b33
Colin McLear <mclear@fastmail.com>
2019-05-11 20:04:54 -0400

    Add dired hacks/ranger package

---
+++ .local/elpa/dired-ranger-20180401.2206/dired-ranger-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dired-ranger" "20180401.2206" "Implementation of useful ranger features for dired" '((dash "2.7.0") (dired-hacks-utils "0.0.1")) :commit "2c1234592aee91dcd9401bcd67213e6a4a464fd9" :keywords '("files") :authors '(("Matúš Goljer" . "matus.goljer@gmail.com")) :maintainer '("Matúš Goljer" . "matus.goljer@gmail.com"))
