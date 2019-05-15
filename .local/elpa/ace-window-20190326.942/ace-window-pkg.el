4a6448fd4a1714419093a45cf1a8886a56d8ffce
Colin McLear <mclear@fastmail.com>
2019-05-15 00:37:00 -0400

    update packages

--- .local/elpa/ace-window-20190326.942/ace-window-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ace-window" "20190326.942" "Quickly switch windows." '((avy "0.2.0")) :commit "301c762f2fe36737ada479d9037776b1ec3741e3" :keywords '("window" "location") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/ace-window")
--- .local/elpa/counsel-20190414.1926/counsel-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "counsel" "20190414.1926" "Various completion functions using Ivy" '((emacs "24.3") (swiper "0.11.0")) :commit "0e62f0d1f61b825ca5eb4b55e47ecb37b3e2834e" :keywords '("convenience" "matching" "tools") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
