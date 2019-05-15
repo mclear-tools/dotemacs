42cd932a5e7cd29d42df005df2c62da37f5ee340
Colin McLear <mclear@fastmail.com>
2019-05-15 00:43:53 -0400

    Update packages

--- .local/elpa/avy-20190511.1400/avy-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "avy" "20190511.1400" "Jump to arbitrary positions in visible text and select text quickly." '((emacs "24.1") (cl-lib "0.5")) :stars '(#("836" 0 3 (font-lock-face paradox-star-face)) #("480K" 0 4 (value 480935 font-lock-face paradox-download-face))) :commit "0e59474430cbfbe7caf2a41cf7935fc613648c49" :keywords '("point" "location") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/avy")
---
+++ .local/elpa/avy-20190514.1551/avy-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "avy" "20190514.1551" "Jump to arbitrary positions in visible text and select text quickly." '((emacs "24.1") (cl-lib "0.5")) :stars '(#("836" 0 3 (font-lock-face paradox-star-face)) #("480K" 0 4 (value 480935 font-lock-face paradox-download-face))) :commit "8686845ed907b986ac7c95169b6396300ee114c2" :keywords '("point" "location") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/avy")
