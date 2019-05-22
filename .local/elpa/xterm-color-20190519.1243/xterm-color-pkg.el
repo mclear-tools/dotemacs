3ce88993bf0eba9dca96fc028503a1eaccabcbe6
Colin McLear <mclear@fastmail.com>
2019-05-22 12:06:48 -0400

    Update packages

---
+++ .local/elpa/xterm-color-20190519.1243/xterm-color-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "xterm-color" "20190519.1243" "ANSI & XTERM 256 color support" '((cl-lib "0.5")) :stars '(#("102" 0 3 (font-lock-face paradox-star-face)) #("101K" 0 4 (value 101836 font-lock-face paradox-download-face))) :commit "7d78e279733cbcc5fc7b064b3dbcdb9f6c01eb16" :keywords '("faces") :authors '(("xristos" . "xristos@sdf.lonestar.org")) :maintainer '("xristos" . "xristos@sdf.lonestar.org") :url "https://github.com/atomontage/xterm-color")
