aa203e638c8648d8dc18ec792759a7f97e2d5fee
Colin McLear <mclear@fastmail.com>
2019-05-31 17:00:57 -0400

    Update packages

--- .local/elpa/counsel-20190523.2008/counsel-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "counsel" "20190523.2008" "Various completion functions using Ivy" '((emacs "24.3") (swiper "0.11.0")) :stars '(#("1168" 0 4 (font-lock-face paradox-star-face)) #("202K" 0 4 (value 202618 font-lock-face paradox-download-face))) :commit "b65cdb52aac4e2fbff0629c0d3f7e72cc927abcd" :keywords '("convenience" "matching" "tools") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
--- .local/elpa/which-key-20190518.157/which-key-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "which-key" "20190518.157" "Display available keybindings in popup" '((emacs "24.4")) :stars '(#("731" 0 3 (font-lock-face paradox-star-face)) #("411K" 0 4 (value 411434 font-lock-face paradox-download-face))) :commit "77586e62b6d306bae07e4e3e093ba488724f4131" :authors '(("Justin Burkett" . "justin@burkett.cc")) :maintainer '("Justin Burkett" . "justin@burkett.cc") :url "https://github.com/justbur/emacs-which-key")
