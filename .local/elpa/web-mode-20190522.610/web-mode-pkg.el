71989339e3207c27f6a4d8175eadcb8a477f52c7
Colin McLear <mclear@fastmail.com>
2019-06-16 09:44:51 -0400

    Update packages

--- .local/elpa/web-mode-20190522.610/web-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "web-mode" "20190522.610" "major mode for editing web templates" '((emacs "23.1")) :stars '(#("1256" 0 4 (font-lock-face paradox-star-face)) #("363K" 0 4 (value 363962 font-lock-face paradox-download-face))) :commit "a67d387f41d45a1bfad694fa06a4fcd504e6e357" :keywords '("languages") :authors '(("François-Xavier Bois <fxbois AT Google Mail Service>")) :maintainer '("François-Xavier Bois") :url "http://web-mode.org")
