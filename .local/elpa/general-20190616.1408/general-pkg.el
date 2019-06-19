9e8a4b60a2dccb311d04a193797402104986afa5
Colin McLear <mclear@fastmail.com>
2019-06-18 22:32:28 -0400

    Update packages

---
+++ .local/elpa/general-20190616.1408/general-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "general" "20190616.1408" "Convenience wrappers for keybindings." '((emacs "24.4") (cl-lib "0.5")) :stars '(#("382" 0 3 (font-lock-face paradox-star-face)) #("15K" 0 3 (value 15580 font-lock-face paradox-download-face))) :commit "ca187fd72f91a3427663256d4b72cb5427d25ad5" :keywords '("vim" "evil" "leader" "keybindings" "keys") :authors '(("Fox Kiester" . "noct@openmailbox.org")) :maintainer '("Fox Kiester" . "noct@openmailbox.org") :url "https://github.com/noctuid/general.el")
