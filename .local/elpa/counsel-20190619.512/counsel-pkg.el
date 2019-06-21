b74067facba8afa51eb4bc11f56340ac94ed878c
Colin McLear <mclear@fastmail.com>
2019-06-20 21:27:27 -0400

    Update packages

---
+++ .local/elpa/counsel-20190619.512/counsel-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "counsel" "20190619.512" "Various completion functions using Ivy" '((emacs "24.3") (swiper "0.11.0")) :stars '("" #(" " 0 1 (value 0 font-lock-face paradox-download-face))) :commit "0b2621975291d3bc764561255f6981ebf95ee0e6" :keywords '("convenience" "matching" "tools") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
---
+++ .local/elpa/treemacs-evil-20190619.1516/treemacs-evil-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "treemacs-evil" "20190619.1516" "Evil mode integration for treemacs" '((evil "1.2.12") (treemacs "0.0")) :stars '("" #(" " 0 1 (value 0 font-lock-face paradox-download-face))) :commit "e01ad21ea3cc0eba8cd460737116b51be32ffb45" :authors '(("Alexander Miller" . "alexanderm@web.de")) :maintainer '("Alexander Miller" . "alexanderm@web.de") :url "https://github.com/Alexander-Miller/treemacs")
