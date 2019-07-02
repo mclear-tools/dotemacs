892eaefa246ddea49a49fb699243e81b850d577c
Colin McLear <mclear@fastmail.com>
2019-07-02 00:33:03 -0400

    Change indenting

--- .local/elisp/setup-config/setup-theme.el
+++ .local/elisp/setup-config/setup-theme.el
@@ -15,110 +15,110 @@
 (use-package solarized-theme
   :if (display-graphic-p)
   :init
-    (progn
+  (progn
     (setq org-todo-keyword-faces
-         '(("TODO" . (:foreground "orange" :weight bold)) ("STARTED" . "yellow")
-           ("WAITING" . (:weight bold))
-           ("SUBMITTED-C" . "green") ("SUBMITTED-J" . "green")
-           ("ACCEPTED-C" . "silver") ("ACCEPTED-J" . "silver")
-           ("REVISE" . (:foreground "violet" :weight bold))))
+          '(("TODO" . (:foreground "orange" :weight bold)) ("STARTED" . "yellow")
+            ("WAITING" . (:weight bold))
+            ("SUBMITTED-C" . "green") ("SUBMITTED-J" . "green")
+            ("ACCEPTED-C" . "silver") ("ACCEPTED-J" . "silver")
+            ("REVISE" . (:foreground "violet" :weight bold))))
 
-        ;; don't make the fringe stand out from the background
-        (setq solarized-distinct-fringe-background nil)
+    ;; don't make the fringe stand out from the background
+    (setq solarized-distinct-fringe-background nil)
 
-        ;; change the font for some headings and titles
-        (setq solarized-use-variable-pitch t)
+    ;; change the font for some headings and titles
+    (setq solarized-use-variable-pitch t)
 
-        ;; make the modeline high contrast
-        (setq solarized-high-contrast-mode-line nil)
-        ;; use this setting without hi contrast modeline
-        (setq x-underline-at-descent-line t)
+    ;; make the modeline high contrast
+    (setq solarized-high-contrast-mode-line nil)
+    ;; use this setting without hi contrast modeline
+    (setq x-underline-at-descent-line t)
 
-        ;; Use bolding
-        (setq solarized-use-less-bold nil)
+    ;; Use bolding
+    (setq solarized-use-less-bold nil)
 
-        ;; Use more italics
-        (setq solarized-use-more-italic t)
+    ;; Use more italics
+    (setq solarized-use-more-italic t)
 
-        ;; Use colors for indicators such as git:gutter, flycheck and similar
-        (setq solarized-emphasize-indicators t)
+    ;; Use colors for indicators such as git:gutter, flycheck and similar
+    (setq solarized-emphasize-indicators t)
 
-        ;; Set to nil of you don't want to change size of org-mode headlines (but keep other size-changes)
-        (setq solarized-scale-org-headlines t)
+    ;; Set to nil of you don't want to change size of org-mode headlines (but keep other size-changes)
+    (setq solarized-scale-org-headlines t)
 
 ;;;; Theme & menubar toggle
-        (setq active-theme 'solarized-dark)
-        (defun toggle-dark-light-theme ()
-        (interactive)
-        (if (eq active-theme 'solarized-light)
-            (progn (setq active-theme 'solarized-dark)
-                   (cpm/osx-menubar-theme-dark)
-                   (add-to-list 'default-frame-alist '(ns-appearance . dark))
-                   (force-mode-line-update))
-            (progn (setq active-theme 'solarized-light)
-                   (cpm/osx-menubar-theme-light)
-                   (add-to-list 'default-frame-alist '(ns-appearance . light))
-                   (force-mode-line-update)))
-        (load-theme active-theme t)))
-        ;; (powerline-reset)))
+    (setq active-theme 'solarized-dark)
+    (defun toggle-dark-light-theme ()
+      (interactive)
+      (if (eq active-theme 'solarized-light)
+          (progn (setq active-theme 'solarized-dark)
+                 (cpm/osx-menubar-theme-dark)
+                 (add-to-list 'default-frame-alist '(ns-appearance . dark))
+                 (force-mode-line-update))
+        (progn (setq active-theme 'solarized-light)
+               (cpm/osx-menubar-theme-light)
+               (add-to-list 'default-frame-alist '(ns-appearance . light))
+               (force-mode-line-update)))
+      (load-theme active-theme t)))
+  ;; (powerline-reset)))
 
-         (progn
-         (defvar after-load-theme-hook nil
-         "Hook run after a color theme is loaded using `load-theme'.")
-         (defadvice load-theme (after run-after-load-theme-hook activate)
-         "Run `after-load-theme-hook'."
-         (run-hooks 'after-load-theme-hook))
+  (progn
+    (defvar after-load-theme-hook nil
+      "Hook run after a color theme is loaded using `load-theme'.")
+    (defadvice load-theme (after run-after-load-theme-hook activate)
+      "Run `after-load-theme-hook'."
+      (run-hooks 'after-load-theme-hook))
 ;;;; Custom Solarized-Dark
-         (defun customize-solarized-dark ()
-         "Customize solarized theme"
-         (if (member 'solarized-dark custom-enabled-themes)
-               (custom-theme-set-faces
-               'solarized-dark
-               ;; make bg darker for higher contrast
-               '(default ((t (:background "#002833" :foreground "#839496"))))
-               ;; matching fringe
-               '(fringe ((t (:background "#002833" :foreground "#586e75"))))
-               ;; fix modeline underline
-                '(mode-line ((t (:background "#073642" :foreground "#839496" :box (:line-width 1 :color "#002833" :style unspecified) :overline "#002833" :underline "#002833"))))
-               ;; terminal
-               '(term ((t (:background "#002833" :foreground "#839496"))))
-               ;; org faces
-               '(org-block ((t (:foreground "#2E8B57"))))
-               '(org-block-begin-line ((t (:foreground "#74a8a4" :weight bold :slant normal))))
-               '(org-block-end-line ((t (:foreground "#74a8a4" :weight bold :slant normal))))
-               '(org-level-1 ((t (:inherit variable-pitch :foreground "#268bd2" :height 1.5))))
-               '(org-level-2 ((t (:inherit variable-pitch :foreground "medium sea green" :height 1.3))))
-               '(org-level-3 ((t (:inherit variable-pitch :foreground "#cb4b16" :height 1.2))))
-               '(org-level-4 ((t (:inherit variable-pitch :foreground "#6c71c4" :height 1.15))))
-               '(org-level-8 ((t (:inherit variable-pitch :foreground "#9e1e86" :height 1.1))))
-               '(org-quote ((t (:inherit org-block :slant normal :weight normal))))
-                '(org-agenda-date ((t (:background "#002833" :foreground "dark cyan" :inverse-video nil :box (:line-width 5 :color "#002833") :overline nil :underline t :slant normal :weight normal :height 1.5 :family "Avenir Next"))))
-               '(org-agenda-date-today ((t (:inherit org-agenda-date :background "#268bd2" :foreground "#002833" :inverse-video t :box nil :overline nil :weight bold))))
-               ;; markdown faces
-               '(markdown-comment-face ((t (:weight normal :slant italic :strike-through nil))))
-               '(markdown-header-face-1 ((t (:inherit variable-pitch :foreground "#268bd2" :height 1.75))))
-               '(markdown-header-face-2 ((t (:inherit variable-pitch :foreground "medium sea green" :height 1.45))))
-               '(markdown-header-face-3 ((t (:inherit variable-pitch :foreground "#cb4b16" :height 1.2))))
-               ;; helm faces
-               '(helm-selection ((t (:background "#073642" :foreground "goldenrod1" :underline nil))))
-               '(helm-match ((t (:foreground "#b58900"))))
-               ;; line number highlighting
-               '(line-number-current-line ((t (:inherit default :foreground "goldenrod1"))))
-               ;; '(nlinum-current-line ((t (:inherit default :foreground "goldenrod1"))))
-               '(linum-highlight-face ((t (:inherit default :foreground "goldenrod1"))))
-                ;; '(nlinum-hl-face ((t (:inherit default :foreground "goldenrod1"))))
-               ;; battery faces
-               '(fancy-battery-charging ((t (:foreground "dark blue" :weight bold))))
-               '(fancy-battery-critical ((t (:foreground "dark red" :weight bold))))
-               '(fancy-battery-discharging ((t (:foreground "dark magenta" :weight bold)))))))
+    (defun customize-solarized-dark ()
+      "Customize solarized theme"
+      (if (member 'solarized-dark custom-enabled-themes)
+          (custom-theme-set-faces
+           'solarized-dark
+           ;; make bg darker for higher contrast
+           '(default ((t (:background "#002833" :foreground "#839496"))))
+           ;; matching fringe
+           '(fringe ((t (:background     "#002833" :foreground "#586e75"))))
+           ;; fix modeline underline
+           '(mode-line ((t (:background "#073642" :foreground "#839496" :box (:line-width 1 :color "#002833" :style unspecified) :overline "#002833" :underline "#002833"))))
+           ;; terminal
+           '(term ((t (:background "#002833" :foreground "#839496"))))
+           ;; org faces
+           '(org-block ((t (:foreground "#2E8B57"))))
+           '(org-block-begin-line ((t (:foreground "#74a8a4" :weight bold :slant normal))))
+           '(org-block-end-line ((t (:foreground "#74a8a4" :weight bold :slant normal))))
+           '(org-level-1 ((t (:inherit variable-pitch :foreground "#268bd2" :height 1.5))))
+           '(org-level-2 ((t (:inherit variable-pitch :foreground "medium sea green" :height 1.3))))
+           '(org-level-3 ((t (:inherit variable-pitch :foreground "#cb4b16" :height 1.2))))
+           '(org-level-4 ((t (:inherit variable-pitch :foreground "#6c71c4" :height 1.15))))
+           '(org-level-8 ((t (:inherit variable-pitch :foreground "#9e1e86" :height 1.1))))
+           '(org-quote ((t (:inherit org-block :slant normal :weight normal))))
+           '(org-agenda-date ((t (:background "#002833" :foreground "dark cyan" :inverse-video nil :box (:line-width 5 :color "#002833") :overline nil :underline t :slant normal :weight normal :height 1.5 :family "Avenir Next"))))
+           '(org-agenda-date-today ((t (:inherit org-agenda-date :background "#268bd2" :foreground "#002833" :inverse-video t :box nil :overline nil :weight bold))))
+           ;; markdown faces
+           '(markdown-comment-face ((t (:weight normal :slant italic :strike-through nil))))
+           '(markdown-header-face-1 ((t (:inherit variable-pitch :foreground "#268bd2" :height 1.75))))
+           '(markdown-header-face-2 ((t (:inherit variable-pitch :foreground "medium sea green" :height 1.45))))
+           '(markdown-header-face-3 ((t (:inherit variable-pitch :foreground "#cb4b16" :height 1.2))))
+           ;; helm faces
+           '(helm-selection ((t (:background "#073642" :foreground "goldenrod1" :underline nil))))
+           '(helm-match ((t (:foreground "#b58900"))))
+           ;; line number highlighting
+           '(line-number-current-line ((t (:inherit default :foreground "goldenrod1"))))
+           ;; '(nlinum-current-line ((t (:inherit default :foreground "goldenrod1"))))
+           '(linum-highlight-face ((t (:inherit default :foreground "goldenrod1"))))
+           ;; '(nlinum-hl-face ((t (:inherit default :foreground "goldenrod1"))))
+           ;; battery faces
+           '(fancy-battery-charging ((t (:foreground "dark blue" :weight bold))))
+           '(fancy-battery-critical ((t (:foreground "dark red" :weight bold))))
+           '(fancy-battery-discharging ((t (:foreground "dark magenta" :weight bold)))))))
 
-          (add-hook 'after-load-theme-hook 'customize-solarized-dark)
+    (add-hook 'after-load-theme-hook 'customize-solarized-dark)
 
 ;;;; Custom Solarized Light
-     (defun customize-solarized-light ()
-     "Customize solarized theme"
-     (if (member 'solarized-light custom-enabled-themes)
-           (custom-theme-set-faces
+    (defun customize-solarized-light ()
+      "Customize solarized theme"
+      (if (member 'solarized-light custom-enabled-themes)
+          (custom-theme-set-faces
            'solarized-light
            ;; fix modeline underline
            '(mode-line ((t (:background "#eee8d5" :foreground "#657b83" :box (:line-width 1 :color "#fdf6e3" :style unspecified) :overline "#fdf6e3" :underline "#fdf6e3"))))
@@ -156,10 +156,10 @@
            '(fancy-battery-charging ((t (:foreground "dark blue" :weight bold))))
            '(fancy-battery-critical ((t (:foreground "dark red" :weight bold))))
            '(fancy-battery-discharging ((t (:foreground "dark magenta" :weight bold))))))
-               )
+      )
 
-          (add-hook 'after-load-theme-hook 'customize-solarized-light))
-          (load-theme 'solarized-dark t))
+    (add-hook 'after-load-theme-hook 'customize-solarized-light))
+  (load-theme 'solarized-dark t))
 
 ;;; Other Themes
 
