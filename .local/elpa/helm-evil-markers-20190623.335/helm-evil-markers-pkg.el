fffa2930d212e615525796be99b4ee8549f77731
Colin McLear <mclear@fastmail.com>
2019-07-03 18:08:11 -0400

    Add helm-evil-markers package

---
+++ .local/elpa/helm-evil-markers-20190623.335/helm-evil-markers-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-evil-markers" "20190623.335" "Show evil markers with helm" '((emacs "24.4") (helm "2.0.0") (evil "1.2.10")) :commit "29f9288a73370f26fe431db1472ed948bd63190d" :keywords '("extensions") :authors '(("Bill Xue")) :maintainer '("Bill Xue") :url "https://github.com/xueeinstein/helm-evil-markers")
