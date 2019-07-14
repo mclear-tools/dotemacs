219c5147981b4d061df4f98220b7c02bb2372303
Colin McLear <mclear@fastmail.com>
2019-07-14 09:39:20 -0400

    Update packages

---
+++ .local/elpa/centaur-tabs-20190713.103/centaur-tabs-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "centaur-tabs" "20190713.103" "Provide an out of box configuration to use tabs" '((emacs "24.4") (powerline "2.4") (cl-lib "0.5")) :stars '("" #(" " 0 1 (value 0 font-lock-face paradox-download-face))) :commit "54b307891d010038680b9b45e56e1774fc6ade76" :authors '(("Emmanuel Bustos" . "ema2159@gmail.com")) :maintainer '("Emmanuel Bustos" . "ema2159@gmail.com") :url "https://github.com/ema2159/centaur-tabs")
---
+++ .local/elpa/pandoc-mode-20190711.2122/pandoc-mode-pkg.el
@@ -0,0 +1,12 @@
+(define-package "pandoc-mode" "20190711.2122" "Minor mode for interacting with Pandoc"
+  '((hydra "0.10.0")
+    (dash "2.10.0"))
+  :keywords
+  '("text" "pandoc")
+  :authors
+  '(("Joost Kremers" . "joostkremers@fastmail.fm"))
+  :maintainer
+  '("Joost Kremers" . "joostkremers@fastmail.fm"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
