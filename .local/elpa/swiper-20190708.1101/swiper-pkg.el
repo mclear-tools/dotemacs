ef45b8ca790f4ea9ba80fb53b25a9756ce98a443
Colin McLear <mclear@fastmail.com>
2019-07-20 20:15:16 -0400

    Update packages

--- .local/elpa/swiper-20190708.1101/swiper-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "swiper" "20190708.1101" "Isearch with an overview. Oh, man!" '((emacs "24.1") (ivy "0.11.0")) :stars '(#("1168" 0 4 (font-lock-face paradox-star-face)) #("238K" 0 4 (font-lock-face paradox-download-face value 238371))) :commit "2fa7cec045e52117b01969c7ffa3fc97f1ce92e0" :keywords '("matching") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
