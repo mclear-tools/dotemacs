2391a98d7f26f63320066c91f77ed5923d64d5eb
Colin McLear <mclear@fastmail.com>
2019-08-18 12:58:08 -0400

    Add theme srcry

--- setup-config/setup-theme.el
+++ setup-config/setup-theme.el
@@ -161,15 +161,35 @@
 
 ;;; Packaging Themes
 
-;; (defvar packages-appearance '(doom-themes nord-theme solarized-theme
-;;   zenburn-theme molokai-theme darktooth-theme gotham-theme
-;;   ample-theme material-theme leuven-theme
-;;   spacemacs-theme gruvbox-theme forest-blue-theme flatland-theme
-;;   afternoon-theme cyberpunk-theme darkmine-theme
-;;   tao-theme darkokai-theme jazz-theme suscolors-theme
-;;   omtose-phellack-theme atom-one-dark-theme nubox
-;;   color-theme-sanityinc-tomorrow alect-themes kaolin-themes)
-;; "A list of themes to ensure are installed at launch.")
+;; (defvar packages-appearance '(doom-themes
+;;                               nord-theme
+;;                               solarized-theme
+;;                               zenburn-theme
+;;                               molokai-theme
+;;                               darktooth-theme
+;;                               gotham-theme
+;;                               ample-theme
+;;                               material-theme
+;;                               leuven-theme
+;;                               spacemacs-theme
+;;                               gruvbox-theme
+;;                               forest-blue-theme
+;;                               flatland-theme
+;;                               afternoon-theme
+;;                               cyberpunk-theme
+;;                               darkmine-theme
+;;                               tao-theme
+;;                               darkokai-theme
+;;                               jazz-theme
+;;                               suscolors-theme
+;;                               omtose-phellack-theme
+;;                               atom-one-dark-theme
+;;                               nubox
+;;                               color-theme-sanityinc-tomorrow
+;;                               alect-themes
+;;                               kaolin-themes
+;;                               srcery-theme)
+;;   "A list of themes to ensure are installed at launch.")
 
 ;; (defun appearance-packages-installed-p ()
 ;;   (loop for p in packages-appearance
