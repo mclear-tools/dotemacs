82a69f69bf14544f9c850125b1f483cfeea67620
Colin McLear <mclear@fastmail.com>
2019-08-18 12:53:25 -0400

    Change manner of setting variables

--- init.el
+++ init.el
@@ -82,34 +82,34 @@
 ;;  We're going to define a number of directories that are used throughout this
 ;;  configuration to store different types of files.
 
-(defvar cpm-emacs-dir (expand-file-name user-emacs-directory)
+(defconst cpm-emacs-dir (expand-file-name user-emacs-directory)
   "The path to the emacs.d directory.")
 
-(defvar cpm-local-dir (concat cpm-emacs-dir ".local/")
+(defconst cpm-local-dir (concat cpm-emacs-dir ".local/")
   "Root directory for local Emacs files. Use this as permanent
   storage for files that are safe to share across systems (if
   this config is symlinked across several computers).")
 
-(defvar cpm-temp-dir (concat cpm-local-dir "temp/")
+(defconst cpm-temp-dir (concat cpm-local-dir "temp/")
   "Directory for non-essential file storage. Used by
   `cpm-etc-dir' and `cpm-cache-dir'.")
 
-(defvar cpm-etc-dir (concat cpm-temp-dir "etc/")
+(defconst cpm-etc-dir (concat cpm-temp-dir "etc/")
   "Directory for non-volatile storage. These are not deleted or
   tampered with by emacs functions. Use this for dependencies
   like servers or config files that are stable (i.e. it should be
   unlikely that you need to delete them if something goes
   wrong).")
 
-(defvar cpm-cache-dir (concat cpm-temp-dir "cache/")
+(defconst cpm-cache-dir (concat cpm-temp-dir "cache/")
   "Directory for volatile storage. Use this for transient files
   that are generated on the fly like caches and temporary files.
   Anything that may need to be cleared if there are problems.")
 
-(defvar cpm-elisp-dir (concat cpm-local-dir "elisp/")
+(defconst cpm-elisp-dir (concat cpm-local-dir "elisp/")
   "Where personal elisp packages and scripts are stored.")
 
-(defvar cpm-setup-dir (concat user-emacs-directory "setup-config/")
+(defconst cpm-setup-dir (concat cpm-emacs-dir "setup-config/")
   "Where the setup-init files are stored.")
 
 ;;;; System Variables
@@ -128,14 +128,15 @@
     (make-directory dir t)))
 
 ;; Exec path -- Emacs won't know where to load things without this
-(defvar cpm-local-bin (concat (getenv "HOME") "/bin") "Local execs.")
-(defvar usr-local-bin "/usr/local/bin")
-(defvar usr-local-sbin "/usr/local/sbin")
+(defconst cpm-local-bin (concat (getenv "HOME") "/bin") "Local execs.")
+(defconst usr-local-bin "/usr/local/bin")
+(defconst usr-local-sbin "/usr/local/sbin")
 (setenv "PATH" (concat usr-local-bin ":" usr-local-sbin ":" (getenv "PATH") ":" cpm-local-bin))
 (setq exec-path (append exec-path (list cpm-local-bin usr-local-sbin usr-local-bin)))
 
 ;; Path to init setup files
-(push cpm-setup-dir load-path)
+(eval-when-compile
+  (push cpm-setup-dir load-path))
 
 ;;;; Security
 ;; Properly verify outgoing ssl connections.
@@ -179,8 +180,7 @@
 ;; =package-initialize= because it appends the elpa packages to the end of the
 ;; load path. Otherwise any time a builtin package was required it would have to
 ;; search all of third party paths first.
-(eval-and-compile
-  (setq load-path (append load-path (directory-files package-user-dir t "^[^.]" t))))
+(setq load-path (eval-when-compile (append load-path (directory-files package-user-dir t "^[^.]" t))))
 
 ;;;; Use-Package Settings
 ;; I tell =use-package= to always defer loading packages unless explicitly told
