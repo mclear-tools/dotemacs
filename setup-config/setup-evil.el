8656b56ea707e899567a44b4adda2db1d4796b28
Colin McLear <mclear@fastmail.com>
2019-09-07 12:01:17 -0400

    Consolidate evil and helm setups

--- setup-config/setup-evil.el
+++ setup-config/setup-evil.el
@@ -7,23 +7,23 @@
         evil-want-keybinding nil)
   :config
   (progn
-  ;; Cursor shape and color
+    ;; Cursor shape and color
     (defcustom dotemacs-evil/emacs-cursor
-    "red"
-    "The color of the cursor when in Emacs state."
-    :type 'color
-    :group 'dotemacs-evil)
+      "red"
+      "The color of the cursor when in Emacs state."
+      :type 'color
+      :group 'dotemacs-evil)
 
     (defcustom dotemacs-evil/emacs-insert-mode
-    nil
-    "If non-nil, insert mode will act as Emacs state."
-    :type 'boolean
-    :group 'dotemacs-evil)
+      nil
+      "If non-nil, insert mode will act as Emacs state."
+      :type 'boolean
+      :group 'dotemacs-evil)
 
     ;; move over visual lines like normal lines
     (general-define-key :states '(motion normal)
-           "j"   #'evil-next-visual-line
-           "k"   #'evil-previous-visual-line)
+      "j"   #'evil-next-visual-line
+      "k"   #'evil-previous-visual-line)
 
     (setq evil-search-module 'evil-search)
     (setq evil-magic 'very-magic)
@@ -37,7 +37,7 @@
     (setq evil-motion-state-cursor  '("plum3" box))
     (setq evil-operator-state-cursor '("red" hollow))
     ;; (setq evil-visual-state-tag "VISUAL")
-    ;use insert in commits automatically
+                                        ;use insert in commits automatically
     (add-hook 'git-commit-mode-hook 'evil-insert-state)
     (evil-set-initial-state 'dashboard-mode 'motion)
     (evil-set-initial-state 'messages-buffer-mode 'motion)
@@ -53,6 +53,131 @@
     ;; evil everywhere
     (evil-mode 1)))
 
+;;; Evil Collection
+(use-package evil-collection
+  :ensure t
+  :after evil
+  :hook (evil-after-load-hook . evil-collection-init)
+  :custom (evil-collection-company-use-tng nil)
+  :init
+  (evil-collection-init))
+
+;;; Evil Indent
+(use-package evil-indent-textobject :commands (evil-indent))
+
+;;; Evil Surround
+(use-package evil-surround
+  :defer 1
+  :commands (evil-surround-region evil-surround-change evil-surround-delete)
+  :general
+  (:states '(visual)
+   "s" 'evil-surround-region
+   "S" 'evil-substitute)
+  :config (global-evil-surround-mode 1))
+
+(use-package evil-embrace
+  :after evil-surround
+  :demand t
+  :hook ((markdown org) . embrace-org-mode-hook)
+  :config
+  (with-eval-after-load 'evil-surround
+    (evil-embrace-enable-evil-surround-integration))
+  (setq evil-embrace-show-help-p nil)
+  ;; (add-hook 'org-mode-hook 'embrace-org-mode-hook)
+  (defun embrace-markdown-mode-hook ()
+    (dolist (lst '((?* "*" . "*")
+                   (?\ "\\" . "\\")
+                   (?$ "$" . "$")
+                   (?/ "/" . "/")))
+      (embrace-add-pair (car lst) (cadr lst) (cddr lst))))
+  (add-hook 'markdown-mode-hook 'embrace-markdown-mode-hook))
+
+;;; Evil Comments
+(use-package evil-commentary
+  :commands (evil-commentary evil-commentary-line)
+  :config
+  (evil-commentary-mode))
+
+;;; Evil Undo
+(use-package undo-tree
+  :commands (undo-tree-undo undo-tree-redo undo-tree-visualize)
+  :init
+  (setq undo-tree-visualizer-timestamps t)
+  (setq undo-tree-visualizer-diff t)
+  (setq undo-tree-enable-undo-in-region nil)
+  ;; supposedly causes errors in undo read
+  ;; see https://emacs.stackexchange.com/a/34214/11934
+  (setq undo-tree-enable-undo-in-region nil)
+  ;; stop littering - set undo directory
+  (let ((undo-dir (concat cpm-cache-dir "undo")))
+    (setq undo-tree-history-directory-alist `(("." . ,undo-dir)))
+    (unless (file-directory-p undo-dir)
+      (make-directory undo-dir t)))
+  (setq undo-tree-auto-save-history nil)
+  :config
+  (global-undo-tree-mode))
+
+
+;;; Evil Multi-Cursor
+(use-package evil-visualstar
+  :commands (evil-visualstar/begin-search-forward evil-visualstar/begin-search-backward))
+
+(use-package evil-multiedit
+  :ensure t
+  :after evil-visualstar
+  :config
+  ;; Default keybindings
+  ;; Highlights all matches of the selection in the buffer.
+  (define-key evil-visual-state-map "R" 'evil-multiedit-match-all)
+
+  ;; Match the word under cursor (i.e. make it an edit region). Consecutive presses will
+  ;; incrementally add the next unmatched match.
+  (define-key evil-normal-state-map (kbd "M-d") 'evil-multiedit-match-and-next)
+  ;; Match selected region.
+  (define-key evil-visual-state-map (kbd "M-d") 'evil-multiedit-and-next)
+  ;; Insert marker at point
+  (define-key evil-insert-state-map (kbd "M-d") 'evil-multiedit-toggle-marker-here)
+
+  ;; Same as M-d but in reverse.
+  (define-key evil-normal-state-map (kbd "M-D") 'evil-multiedit-match-and-prev)
+  (define-key evil-visual-state-map (kbd "M-D") 'evil-multiedit-and-prev)
+
+  ;; OPTIONAL: If you prefer to grab symbols rather than words, use
+  ;; `evil-multiedit-match-symbol-and-next` (or prev).
+
+  ;; Restore the last group of multiedit regions.
+  (define-key evil-visual-state-map (kbd "C-M-D") 'evil-multiedit-restore)
+
+  ;; RET will toggle the region under the cursor
+  (define-key evil-multiedit-state-map (kbd "RET") 'evil-multiedit-toggle-or-restrict-region)
+
+  ;; ...and in visual mode, RET will disable all fields outside the selected region
+  (define-key evil-motion-state-map (kbd "RET") 'evil-multiedit-toggle-or-restrict-region)
+
+  ;; For moving between edit regions
+  (define-key evil-multiedit-state-map (kbd "C-n") 'evil-multiedit-next)
+  (define-key evil-multiedit-state-map (kbd "C-p") 'evil-multiedit-prev)
+  (define-key evil-multiedit-insert-state-map (kbd "C-n") 'evil-multiedit-next)
+  (define-key evil-multiedit-insert-state-map (kbd "C-p") 'evil-multiedit-prev)
+
+  ;; Ex command that allows you to invoke evil-multiedit with a regular expression, e.g.
+  (evil-ex-define-cmd "ie[dit]" 'evil-multiedit-ex-match))
+
+(use-package evil-mc
+  :ensure t
+  :commands (evil-mc-make-all-cursors evil-mc-make-and-goto-next-match))
+
+
+;;; Evil Numbers
+(use-package evil-numbers
+  :commands (evil-numbers/inc-at-pt evil-numbers/dec-at-pt)
+  :init
+  (general-define-key
+   :states '(normal visual insert emacs)
+   "H-s" 'evil-numbers/inc-at-pt
+   "H-a" 'evil-numbers/dec-at-pt))
+
+;;; End Evil-Packages
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (provide 'setup-evil)
--- setup-config/setup-helm.el
+++ setup-config/setup-helm.el
@@ -1,4 +1,4 @@
-;;;; Helm
+;;; Helm
 (use-package helm
   :ensure t
   :defer
@@ -113,5 +113,97 @@
       (setq-local cursor-type nil))))
 (add-hook 'helm-minibuffer-set-up-hook #'helm-hide-minibuffer-maybe)
 
+;;; Helm  Packages
+
+(use-package helm-org
+  :ensure t
+  :after helm
+  :commands (helm-org-agenda-files-headings
+             helm-org-in-buffer-headings
+             helm-org-parent-headings
+             helm-org-capture-templates))
+
+(use-package helm-ag
+  :load-path "~/.emacs.d/.local/elisp/helm-ag/"
+  :after helm
+  :demand t
+  :commands (helm-ag helm-ag-buffers helm-ag-this-file helm-do-ag helm-ag-project-root cpm/helm-files-do-ag cpm/helm-files-search-current-directory)
+  :config
+  (setq helm-follow-mode-persistent t)
+  (setq helm-ag-base-command "rg --no-heading")
+  (setq helm-ag-fuzzy-match t))
+
+(use-package helm-descbinds
+  :commands helm-descbinds
+  :config
+  (setq helm-descbinds-window-style 'same-window)
+  (add-hook 'helm-mode-hook 'helm-descbinds-mode))
+
+(use-package helm-ls-git :commands helm-ls-git-ls)
+
+(use-package helm-hunks :commands helm-hunks)
+
+(use-package helm-swoop
+  :commands (helm-swoop-without-pre-input helm-swoop-back-to-last-point helm-multi-swoop helm-multi-swoop-all)
+  :config
+  (setq helm-swoop-use-fuzzy-match t)
+  (setq helm-swoop-split-with-multiple-windows t))
+
+(use-package helm-dired-recent-dirs
+  :commands helm-dired-recent-dirs-view)
+
+(use-package helm-files
+  :ensure nil
+  :defer t
+  :config
+  (setq helm-ff-skip-boring-files t)
+  (setq helm-idle-delay 0.05)
+  (setq helm-input-idle-delay 0.05)
+  (setq helm-ff-file-name-history-use-recentf t)
+  (setq helm-boring-file-regexp-list
+        '("\\.git$" "\\.hg$" "\\.svn$" "\\.CVS$" "\\._darcs$" "\\.la$" "\\.o$" "~$"
+          "\\.so$" "\\.a$" "\\.elc$" "\\.fas$" "\\.fasl$" "\\.pyc$" "\\.pyo$")))
+
+(use-package helm-themes
+  :commands helm-themes)
+
+(defadvice helm-themes--load-theme (after helm-themes--load-theme-after activate) (require 'powerline) (powerline-reset))
+;; (ad-unadvise 'helm-themes--load-theme)
+
+;;;; Search info manuals with helm
+;;   Search the Emacs, Emacs Lisp, and Emacs Common Lisp manuals, all at the same time
+;; Courtesy of alphapapa and reddit:
+;; https://www.reddit.com/r/emacs/comments/bl3dsb/search_entire_info_manual_with_swiper/emmxsxg?utm_source=share&utm_medium=web2x
+(defun cpm/helm-info-emacs-elisp-cl ()
+  "Helm for Emacs, Elisp, and CL-library info pages."
+  (interactive)
+  (helm :sources '(helm-source-info-emacs
+                   helm-source-info-eintr
+                   helm-source-info-cl)))
+
+;;;; Helm-markers
+(use-package helm-evil-markers
+  :ensure t
+  :after (evil helm)
+  :demand t
+  :config
+  ;; enable helm-evil-markers
+  (helm-evil-markers-toggle))
+
+;;;; Helm-preview
+;; Preview files selected in helm
+(use-package helm-file-preview
+  :ensure t
+  :after helm
+  :demand t
+  :config
+  ;; turn off to have preview no matter what
+  (setq helm-file-preview-only-when-line-numbers t)
+  ;; set to nil to leave file open
+  (setq helm-file-preview-preview-only t)
+  (helm-file-preview-mode t))
+
+;;; End Helm Setup
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (provide 'setup-helm)
