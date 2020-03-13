b0bc806090010159aba81698a9b42180f4b886b5
Colin McLear <mclear@fastmail.com>
2020-03-13 16:00:14 -0400

    Revise async package setup
    - add function to limit async processes
      - see https://gist.github.com/kiennq/cfe57671bab3300d3ed849a7cbf2927c

--- setup-config/setup-libraries.el
+++ setup-config/setup-libraries.el
@@ -4,7 +4,38 @@
 ;; libraries for asynchronous processing string manipulation, list manipulation and
 ;; backward compatibility respectively. The git package is also a library.
 
-(use-package async   :defer 2)
+(use-package async
+  :ensure t
+  :defer 2
+  :init
+  (setq async-bytecomp-allowed-packages '(all))
+  :custom
+  ;; async compiling package
+  (async-bytecomp-package-mode 1)
+  :config
+  (dired-async-mode 1)
+  ;; limit number of async processes
+  (eval-when-compile
+    (require 'cl-lib))
+  (defvar async-maximum-parallel-procs 4)
+  (defvar async--parallel-procs 0)
+  (defvar async--queue nil)
+  (defvar-local async--cb nil)
+  (advice-add #'async-start :around
+              (lambda (orig-func func &optional callback)
+                (if (>= async--parallel-procs async-maximum-parallel-procs)
+                    (push `(,func ,callback) async--queue)
+                  (cl-incf async--parallel-procs)
+                  (let ((future (funcall orig-func func
+                                         (lambda (re)
+                                           (cl-decf async--parallel-procs)
+                                           (when async--cb (funcall async--cb re))
+                                           (when-let (args (pop async--queue))
+                                             (apply #'async-start args))))))
+                    (with-current-buffer (process-buffer future)
+                      (setq async--cb callback)))))
+              '((name . --queue-dispatch)))
+  )
 (use-package dash    :defer 2)
 (use-package s       :defer 2)
 (use-package f       :defer 2)
