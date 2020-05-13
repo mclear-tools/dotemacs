517b7105140412dfbb8eb00de93f54ba1a20ff9d
Colin McLear <mclear@fastmail.com>
2020-05-12 22:19:07 -0400

    Add roboto mono font

--- setup-config/setup-ui.el
+++ setup-config/setup-ui.el
@@ -26,6 +26,7 @@
 (defvar cpm-font3 (font-spec :family "DejaVuSansMono Nerd Font" :size 13))
 (defvar cpm-font4 (font-spec :family "SauceCodePro Nerd Font" :size 13))
 (defvar cpm-font5 (font-spec :family "FiraCode Nerd Font" :size 13))
+(defvar cpm-font5 (font-spec :family "RobotoMono Nerd Font" :size 14))
 (defvar cpm-ligatures t)
 (defvar cpm-vari-font (font-spec :family "Avenir Next"))
 (defvar cpm-unicode-font (font-spec :family "Symbola"))
