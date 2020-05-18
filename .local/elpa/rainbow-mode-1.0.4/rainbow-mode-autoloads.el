0b46758cc2ec8779023bc2fd4e70e684d24bc536
Colin McLear <mclear@fastmail.com>
2020-05-17 23:06:30 -0400

    Reinstall rainbow-mode
    - the last install was blank for some reason

--- .local/elpa/rainbow-mode-1.0.4/rainbow-mode-autoloads.el
+++ .local/elpa/rainbow-mode-1.0.4/rainbow-mode-autoloads.el
@@ -6,6 +6,19 @@
                          (or (file-name-directory #$) (car load-path))))
 
 
+;;;### (autoloads nil "rainbow-mode" "rainbow-mode.el" (0 0 0 0))
+;;; Generated autoloads from rainbow-mode.el
+
+(autoload 'rainbow-mode "rainbow-mode" "\
+Colorize strings that represent colors.
+This will fontify with colors the string like \"#aabbcc\" or \"blue\".
+
+\(fn &optional ARG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "rainbow-mode" '("rainbow-")))
+
+;;;***
+
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
