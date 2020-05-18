bee1ef4fccfa78b5d25ff12699498d8958b02f4a
Colin McLear <mclear@fastmail.com>
2020-05-17 23:21:33 -0400

    Try fixing indentation again
    - trying a doom config for undo-fu to see if there are fewer errors

--- setup-config/setup-evil.el
+++ setup-config/setup-evil.el
@@ -53,10 +53,14 @@
     (setq evil-move-cursor-back nil)
     ;; move by visual lines
     (setq evil-respect-visual-line-mode t)
-    ;; allow evil-char move across lines
-    (setq evil-cross-lines t)
+    ;; whether to allow evil-char move across lines
+    (setq evil-cross-lines nil)
     ;; Use counsel to provide :ls
     (evil-ex-define-cmd "buffers" 'counsel-ibuffer)
+    ;; fine-grained undo
+    (setq evil-want-fine-undo t)
+    ;; disable undo-tree
+    (global-undo-tree-mode -1)
     ;; evil everywhere
     (evil-mode 1)))
 
@@ -110,47 +114,64 @@
 ;; https://github.com/emacs-evil/evil/issues/1074 and
 ;; http://ergoemacs.org/emacs/emacs_best_redo_mode.html
 ;; So disable undo-tree; have to do this manually b/c it is automatically loaded by evil
+
+;; so many problems with this package...
+;; https://github.com/emacs-evil/evil/issues/1074
 (use-package undo-tree
-  :after evil
+  ;; :commands (undo-tree-undo undo-tree-redo undo-tree-visualize)
+  :disabled
+  :general
+  (:states '(normal)
+           "u" 'undo-tree-undo)
+  (:states '(normal insert motion emacs)
+           "s-z" 'undo-tree-undo
+           "s-Z" 'undo-tree-redo)
   :init
-  (global-undo-tree-mode -1))
+  (setq undo-tree-visualizer-timestamps t)
+  (setq undo-tree-visualizer-diff t)
+  (setq undo-tree-enable-undo-in-region nil)
+  ;; supposedly causes errors in undo read
+  ;; see https://emacs.stackexchange.com/a/34214/11934
+  (setq undo-tree-enable-undo-in-region nil)
+  ;; stop littering - set undo directory
+  (let ((undo-dir (concat cpm-cache-dir "undo")))
+    (setq undo-tree-history-directory-alist `(("." . ,undo-dir)))
+    (unless (file-directory-p undo-dir)
+      (make-directory undo-dir t)))
+  (setq undo-tree-auto-save-history t)
+  :config
+  (global-undo-tree-mode))
 
-;; (use-package undo-tree
-;;   ;; :commands (undo-tree-undo undo-tree-redo undo-tree-visualize)
-;;   :init
-;;   (setq undo-tree-visualizer-timestamps t)
-;;   (setq undo-tree-visualizer-diff t)
-;;   (setq undo-tree-enable-undo-in-region nil)
-;;   ;; supposedly causes errors in undo read
-;;   ;; see https://emacs.stackexchange.com/a/34214/11934
-;;   (setq undo-tree-enable-undo-in-region nil)
-;;   ;; stop littering - set undo directory
-;;   (let ((undo-dir (concat cpm-cache-dir "undo")))
-;;     (setq undo-tree-history-directory-alist `(("." . ,undo-dir)))
-;;     (unless (file-directory-p undo-dir)
-;;       (make-directory undo-dir t)))
-;;   (setq undo-tree-auto-save-history nil)
-;;   :config
-;;   (global-undo-tree-mode -1))
-
+;;; Undo-Fu
 ;; trying another undo package
 ;; https://gitlab.com/ideasman42/emacs-undo-fu
 (use-package undo-fu
+  :after evil
+  :demand t
+  :init
+  ;; `evil' activates undo-tree, so we must pre-emptively disable it.
+  (after! undo-tree
+    (global-undo-tree-mode -1))
   :general
   (:states '(normal)
-   "u" 'undo-fu-only-undo
-   "\C-r" 'undo-fu-only-redo)
+           "u" 'undo-fu-only-undo
+           "\C-r" 'undo-fu-only-redo)
   (:states '(normal insert motion emacs)
-   "s-z" 'undo-fu-only-undo
-   "s-Z" 'undo-fu-only-redo))
+           "s-z" 'undo-fu-only-undo
+           "s-Z" 'undo-fu-only-redo)
+  :config
+  ;; Store more undo history to prevent loss of data
+  (setq undo-limit 400000
+        undo-strong-limit 3000000
+        undo-outer-limit 3000000))
 
 ;; persistent undo across sessions
 (use-package undo-fu-session
   :after undo-fu
   :demand t
   :config
-  (setq undo-fu-session-file-limit 50)
-  (setq undo-fu-session-directory (concat cpm-cache-dir "undo-fu-session"))
+  (setq undo-fu-session-file-limit nil)
+  (setq undo-fu-session-directory (concat cpm-cache-dir "undo-fu-session/"))
   (setq undo-fu-session-incompatible-files '("/COMMIT_EDITMSG\\'" "/git-rebase-todo\\'")))
 (with-eval-after-load 'undo
   (global-undo-fu-session-mode))
