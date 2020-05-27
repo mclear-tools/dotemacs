05d6a20ababc81a6c5cb4c723a575decaf67f04d
Colin McLear <mclear@fastmail.com>
2020-05-27 11:51:10 -0400

    Add function for table wrap & fix grab link for org & firefox

--- setup-config/setup-org.el
+++ setup-config/setup-org.el
@@ -418,7 +418,7 @@ _vr_ reset      ^^                       ^^                 ^^
           ("j" "Journal" entry (file+olp+datetree ,(concat org-directory "journal.org"))
            "**** %<%H:%M>\n%?")
           ("l" "A link, for reading later" entry (file ,(concat org-directory "inbox.org"))
-           "* %? :link: \n%(grab-mac-link 'safari 'org)")
+           "* %? :link: \n%(grab-mac-link 'firefox 'org)")
           ("m" "Mail-Task" entry (file ,(concat org-directory "inbox.org"))
            "* TODO %? :email: \n%(grab-mac-link 'mail 'org)")
           ;; ("m" "Mail-Task" entry (file ,(concat org-directory "inbox.org"))
@@ -1461,6 +1461,60 @@ is non-nil."
                       (org-link-escape (org-link-decode uri)))))))))))
 
 
+;;;; Org Table Wrap
+;; see https://emacs.stackexchange.com/a/30871/11934
+(defun org-table-wrap-to-width (width)
+  "Wrap current column to WIDTH."
+  (interactive (list (read-number "Enter column width: ")))
+  (org-table-check-inside-data-field)
+  (org-table-align)
+
+  (let (cline (ccol (org-table-current-column)) new-row-count (more t))
+    (org-table-goto-line 1)
+    (org-table-goto-column ccol)
+
+    (while more
+      (setq cline (org-table-current-line))
+
+      ;; Cut current field
+      (org-table-copy-region (point) (point) 'cut)
+
+      ;; Justify for width
+      (setq org-table-clip
+            (mapcar 'list (org-wrap (caar org-table-clip) width nil)))
+
+      ;; Add new lines and fill
+      (setq new-row-count (1- (length org-table-clip)))
+      (if (> new-row-count 0)
+          (org-table-insert-n-row-below new-row-count))
+      (org-table-goto-line cline)
+      (org-table-goto-column ccol)
+      (org-table-paste-rectangle)
+      (org-table-goto-line (+ cline new-row-count))
+
+      ;; Move to next line
+      (setq more (org-table-goto-line (+ cline new-row-count 1)))
+      (org-table-goto-column ccol))
+
+    (org-table-goto-line 1)
+    (org-table-goto-column ccol)))
+
+(defun org-table-insert-n-row-below (n)
+  "Insert N new lines below the current."
+  (let* ((line (buffer-substring (point-at-bol) (point-at-eol)))
+         (new (org-table-clean-line line)))
+    ;; Fix the first field if necessary
+    (if (string-match "^[ \t]*| *[#$] *|" line)
+        (setq new (replace-match (match-string 0 line) t t new)))
+    (beginning-of-line 2)
+    (setq new
+          (apply 'concat (make-list n (concat new "\n"))))
+    (let (org-table-may-need-update) (insert-before-markers new))  ;;; remove?
+    (beginning-of-line 0)
+    (re-search-forward "| ?" (point-at-eol) t)
+    (and (or org-table-may-need-update org-table-overlay-coordinates) ;;; remove?
+         (org-table-align))
+    (org-table-fix-formulas "@" nil (1- (org-table-current-dline)) n)))
 ;;; Org-Reveal
 (use-package ox-reveal
   :commands (org-reveal-export-current-subtree org-reveal-export-to-html-and-browse)
