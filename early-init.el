ae93e914e74aac7e52ffd4725426b2902db633d4
Colin McLear <mclear@fastmail.com>
2021-01-02 23:52:43 -0500

    Revise early init file

--- early-init.el
+++ early-init.el
@@ -1,11 +1,124 @@
-;; early-init.el  -*- lexical-binding: t; mode: emacs-lisp; coding:utf-8; fill-column: 80 -*-
+;; early-init.el  -*- lexical-binding: t; no-byte-compile: t; mode: emacs-lisp; coding:utf-8; fill-column: 80 -*-
 ;; Only for use with emacs 27 or higher
 ;; see https://www.reddit.com/r/emacs/comments/7yns85/emacs_adds_support_for_a_second_read_earlier_init/
 ;; and https://lists.gnu.org/archive/html/emacs-devel/2017-10/msg00372.html
 ;; for more information
 
+;;; Early Startup
+;;;; Speed up startup
+;; Help speed up emacs initialization
+;; See https://blog.d46.us/advanced-emacs-startup/
+;; and http://tvraman.github.io/emacspeak/blog/emacs-start-speed-up.html
+;; and https://www.reddit.com/r/emacs/comments/3kqt6e/2_easy_little_known_steps_to_speed_up_emacs_start/
+
+(defvar cpm--file-name-handler-alist file-name-handler-alist)
+(setq file-name-handler-alist nil)
+
+;;;; Garbage collection
+;; Adjust garbage collection thresholds during startup, and thereafter
+;; see http://akrl.sdf.org
+;; https://gitlab.com/koral/gcmh
+;; NOTE: The system linked above generates too many GC pauses so I'm using my own mixed setup
+;; https://github.com/purcell/emacs.d/blob/3b1302f2ce3ef2f69641176358a38fd88e89e664/init.el#L24
+
+(let ((normal-gc-cons-threshold (* 20 1024 1024))
+      (init-gc-cons-threshold (* 128 1024 1024)))
+  (setq gc-cons-threshold init-gc-cons-threshold)
+  (add-hook 'emacs-startup-hook
+            (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
+
+(defmacro k-time (&rest body)
+  "Measure and return the time it takes evaluating BODY."
+  `(let ((time (current-time)))
+     ,@body
+     (float-time (time-since time))))
+
+;; When idle for 15sec run the GC no matter what.
+(defvar k-gc-timer
+  (run-with-idle-timer 15 t
+                       (lambda ()
+                         (message "Garbage Collector has run for %.06fsec"
+                                  (k-time (garbage-collect))))))
+
+
+;;;; Prefer Newer files
+;; prefer newer versions
+(setq load-prefer-newer t)
+
+;;;; Byte Compile Warnings
+;; Disable certain byte compiler warnings to cut down on the noise. This is a
+;; personal choice and can be removed if you would like to see any and all byte
+;; compiler warnings.
+(setq byte-compile-warnings '(not free-vars unresolved noruntime lexical make-local obsolete))
+;;;; Check Errors
+;; Produce backtraces when errors occur
+(setq debug-on-error nil)
+
+;;;; When-let errors
+;; https://github.com/alphapapa/frame-purpose.el/issues/3
+;; https://github.com/alphapapa/frame-purpose.el/issues/3
+(eval-and-compile
+  (when (version< emacs-version "26")
+    (with-no-warnings
+      (defalias 'when-let* #'when-let)
+      (function-put #'when-let* 'lisp-indent-function 1)
+      (defalias 'if-let* #'if-let)
+      (function-put #'if-let* 'lisp-indent-function 2))))
+
+;;;; Variable Binding Depth
+;; https://www.reddit.com/r/emacs/comments/9jp9zt/anyone_know_what_variable_binding_depth_exceeds/
+(setq max-specpdl-size 13000)
+
+
+;;; Package settings
 ;; (setq package-quickstart t)
 ;; (setq package-quickstart-file "~/.emacs.d/.local/temp/cache/package-quickstart.el")
 (setq package-enable-at-startup nil)
+;; for native comp
+;; see https://github.com/jimeh/build-emacs-for-macos#native-comp
+(setq comp-speed 2)
 
-;;; Early Startup
+
+;;; Clean View
+;; Disable start-up screen
+(setq-default inhibit-startup-screen t)
+(setq inhibit-splash-screen t)
+(setq inhibit-startup-message t)
+(setq initial-scratch-message "")
+(setq frame-inhibit-implied-resize t)
+
+;; UI - Disable visual cruft
+(unless (eq window-system 'ns)
+  (menu-bar-mode -1))
+(when (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))
+(when (fboundp 'scroll-bar-mode)
+  (scroll-bar-mode -1))
+(when (fboundp 'horizontal-scroll-bar-mode)
+  (horizontal-scroll-bar-mode -1))
+
+;; Quick start scratch buffer
+(setq initial-major-mode 'fundamental-mode)
+
+;; echo buffer
+;; Don't display any message
+;; https://emacs.stackexchange.com/a/437/11934
+(defun display-startup-echo-area-message ()
+  (message ""))
+
+;; And bury the scratch buffer, don't kill it
+(defadvice kill-buffer (around kill-buffer-around-advice activate)
+  (let ((buffer-to-kill (ad-get-arg 0)))
+    (if (equal buffer-to-kill "*scratch*")
+        (bury-buffer)
+      ad-do-it)))
+
+
+;; No frame title header
+(setq-default frame-title-format nil)
+;; (setq frame-title-format "\n")
+;; (setq frame-title-format
+;;       '((buffer-file-name "%f" "%b")))
+
+;; maximize frame
+(add-to-list 'initial-frame-alist '(fullscreen . maximized))
