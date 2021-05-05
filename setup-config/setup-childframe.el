3bda5132f3f061b382e9f74cfc40f824a1d50b0e
Colin McLear <mclear@fastmail.com>
2021-05-05 00:27:38 -0400

    Rename posframe to childframe

--- setup-config/setup-childframe.el
+++ setup-config/setup-childframe.el
@@ -1,7 +1,57 @@
 ;; Open a child frame.
 
+;;; Miniframe
+;; NOTE: provides a great ui for completion, similar to posframe
+(use-package mini-frame
+  :straight (:type git :host github :repo "muffinmad/emacs-mini-frame")
+  :hook (after-init . mini-frame-mode)
+  :commands (mini-frame-mode)
+  :custom
+  (mini-frame-show-parameters `((top    . 0.023)
+                                (width  . 0.98)
+                                (left   . 0.5)
+                                (height . 10)
+                                (child-frame-border-width . 15)
+                                (internal-border-width . 0)
+                                (left-fringe . 20)
+                                (right-fringe . 20)
+                                ;; set colors for bespoke theme
+                                (foreground-color . ,bespoke-strong)
+                                (background-color . ,bespoke-subtle)
+                                ))
+  (mini-frame-color-shift-step 7)
+  (mini-frame-advice-functions '(read-from-minibuffer
+                                 read-string
+                                 completing-read))
+  (mini-frame-resize nil)
+  :config
+  (defun cpm/reload-child-frame ()
+    "function to reload child-frame settings"
+    (interactive)
+    (load-library "setup-childframe"))
+  ;; add hook to reload mini-frame colors on bespoke-theme change
+  (add-hook 'after-load-theme-hook 'cpm/reload-child-frame))
+
 ;;; Posframe
 (use-package posframe)
+;;; Transient Posframe
+(use-package transient-posframe
+  :straight (:host github :repo "yanghaoxie/transient-posframe")
+  :if (and (window-system) (version<= "26.1" emacs-version))
+  :hook (magit-mode . transient-posframe-mode))
+
+;;; Which-Key Posframe
+(use-package which-key-posframe
+  :if (and (window-system) (version<= "26.1" emacs-version))
+  :hook (after-init . which-key-posframe-mode)
+  :config
+  (setq posframe-arghandler #'cpm/posframe-arghandler)
+  ;; see https://github.com/yanghaoxie/which-key-posframe/issues/5#issuecomment-527528759
+  (defun cpm/posframe-arghandler (buffer-or-name arg-name value)
+    (let ((info '(:width (round (* (frame-width) 0.72)) :height 75)))
+      (or (plist-get info arg-name) value)))
+  (setq which-key-posframe-border-width 15)
+  (setq which-key-posframe-poshandler 'posframe-poshandler-frame-top-center))
 
 ;;; Selectrum Posframe
 ;; (setq selectrum-display-action '(display-buffer-show-in-posframe))
@@ -20,6 +70,7 @@
 
 ;;; Helm posframe
 (use-package helm-posframe
+  :disabled
   :if (and (window-system) (version<= "26.1" emacs-version))
   :hook (after-init . helm-posframe-enable)
   :init
@@ -35,6 +86,7 @@
 
 ;;; Ivy Posframe
 (use-package ivy-posframe
+  :disabled
   :if (and (window-system) (version<= "26.1" emacs-version))
   :hook
   (ivy-mode . ivy-posframe-mode)
@@ -71,20 +123,6 @@
   :hook (company-mode . company-posframe-mode))
 
 
-;;; Which-Key Posframe
-(use-package which-key-posframe
-  ;; :disabled
-  :if (and (window-system) (version<= "26.1" emacs-version))
-  :hook (after-init . which-key-posframe-mode)
-  :config
-  (setq posframe-arghandler #'cpm/posframe-arghandler)
-  ;; see https://github.com/yanghaoxie/which-key-posframe/issues/5#issuecomment-527528759
-  (defun cpm/posframe-arghandler (buffer-or-name arg-name value)
-    (let ((info '(:width (round (* (frame-width) 0.72)) :height 75)))
-      (or (plist-get info arg-name) value)))
-  (setq which-key-posframe-border-width 15)
-  (setq which-key-posframe-poshandler 'posframe-poshandler-frame-center))
-
 ;;; Hydra Posframe
 ;; doesn't work well with ivy hydras
 ;; (eval-when-compile
@@ -97,6 +135,5 @@
   :config
   (setq hydra-posframe-border-width 15))
 
-
 ;;; End posframe
-(provide 'setup-posframe)
+(provide 'setup-childframe)
