8625842fef76f29fc5941f88f2ae495a3004584d
Colin McLear <mclear@fastmail.com>
2021-05-17 21:48:42 -0400

    Replace slectrum w/vertico

--- setup-config/setup-completion.el
+++ setup-config/setup-completion.el
@@ -29,11 +29,45 @@
   (icomplete-mode)
   (icomplete-vertical-mode))
 
+;;;; Vertico
+(use-package vertico
+  :general
+  (:keymaps 'vertico-map
+   "C-j"    'vertico-next
+   "C-k"    'vertico-previous)
+  :init
+  (vertico-mode)
+  :config
+  ;; Optionally enable cycling for `vertico-next' and `vertico-previous'.
+  (setq vertico-cycle t))
+
+;; A few more useful configurations...
+(use-package emacs
+  :init
+  ;; Add prompt indicator to `completing-read-multiple'.
+  (defun crm-indicator (args)
+    (cons (concat "[CRM] " (car args)) (cdr args)))
+  (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
+
+  ;; Grow and shrink minibuffer
+  ;;(setq resize-mini-windows t)
+
+  ;; Do not allow the cursor in the minibuffer prompt
+  (setq minibuffer-prompt-properties
+        '(read-only t cursor-intangible t face minibuffer-prompt))
+  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
+
+  ;; Enable recursive minibuffers
+  (setq enable-recursive-minibuffers t))
+
+;; Persist history over Emacs restarts using savehist (see setup-settings). Vertico sorts by history position.
+
 ;;;; Selectrum
 ;; selectrum is nice but I think I like Icomplete better
 ;; Icomplete is less buggy and orderless works well.
 
 (use-package selectrum
+  :disabled t
   :straight t
   :hook (after-init . selectrum-mode)
   :general
@@ -55,6 +89,7 @@
   (selectrum-mode +1))
 
 (use-package selectrum-prescient
+  :disabled
   :straight t
   :after selectrum
   :config
@@ -65,12 +100,21 @@
 
 ;;;; Orderless
 ;; ordering of narrowed candidates
+;; Use the `orderless' completion style.
+;; Enable `partial-completion' for files to allow path expansion.
+;; You may prefer to use `initials' instead of `partial-completion'.
 (use-package orderless
-  :straight t
-  :after selectrum
-  :config
-  (setq completion-styles '(orderless))
-  (setq orderless-skip-highlighting (lambda () selectrum-is-active)))
+  :init
+  (setq completion-styles '(orderless)
+        completion-category-defaults nil
+        completion-category-overrides '((file (styles . (partial-completion))))))
+
+;; (use-package orderless
+;;   :straight t
+;;   :after selectrum
+;;   :config
+;;   (setq completion-styles '(orderless))
+;;   (setq orderless-skip-highlighting (lambda () selectrum-is-active)))
 
 
 ;;;; Embark
@@ -179,6 +223,10 @@
   (interactive)
   (with-persp-buffer-list () (consult-buffer)))
 
+;; Search at point with consult
+(defun consult-line-symbol-at-point ()
+  (interactive)
+  (consult-line (thing-at-point 'symbol)))
 
 ;; Optionally configure narrowing key.
 ;; Both < and C-+ work reasonably well.
