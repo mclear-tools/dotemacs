078b7272ebb91897126cbeb3512b1f47087aeb9b
Colin McLear <mclear@fastmail.com>
2021-05-24 00:06:00 -0400

    Revise mini-frame settings & remove old posframe cruft

--- setup-config/setup-childframe.el
+++ setup-config/setup-childframe.el
@@ -1,7 +1,7 @@
 ;; Open a child frame.
 
 ;;; Miniframe
-;; NOTE: provides a great ui for completion, similar to posframe
+;; Provides a great ui for completion, similar to posframe
 (use-package mini-frame
   :straight (:type git :host github :repo "muffinmad/emacs-mini-frame")
   :hook (after-init . mini-frame-mode)
@@ -10,7 +10,7 @@
   (mini-frame-show-parameters `((top    . 0.023)
                                 (width  . 0.98)
                                 (left   . 0.5)
-                                (height . 10)
+                                (height . 11)
                                 (child-frame-border-width . 15)
                                 (internal-border-width . 0)
                                 (left-fringe . 20)
@@ -19,7 +19,7 @@
                                 (foreground-color . ,bespoke-strong)
                                 (background-color . ,bespoke-subtle)
                                 ))
-  (mini-frame-color-shift-step 7)
+  ;; (mini-frame-color-shift-step 7)
   (mini-frame-advice-functions '(read-from-minibuffer
                                  read-string
                                  completing-read))
@@ -59,90 +59,13 @@
   (defun cpm/posframe-arghandler (buffer-or-name arg-name value)
     (let ((info '(:width (round (* (frame-width) 0.72)) :height 75)))
       (or (plist-get info arg-name) value)))
-  (setq which-key-posframe-border-width 15)
+  (setq which-key-posframe-border-width 20)
   (setq which-key-posframe-poshandler 'posframe-poshandler-frame-top-center))
 
-;;; Selectrum Posframe
-;; (setq selectrum-display-action '(display-buffer-show-in-posframe))
-
-(defun display-buffer-show-in-posframe (buffer _alist)
-  (frame-root-window
-   (posframe-show buffer
-                  :min-height 10
-                  :min-width (round (* (frame-width) 0.75))
-                  :internal-border-width 15
-                  :left-fringe 0
-                  :right-fringe 0
-                  :poshandler 'posframe-poshandler-frame-center)))
-
-;; (add-hook 'minibuffer-exit-hook 'posframe-delete-all)
-
-;;; Helm posframe
-(use-package helm-posframe
-  :disabled
-  :if (and (window-system) (version<= "26.1" emacs-version))
-  :hook (after-init . helm-posframe-enable)
-  :init
-  (add-hook 'helm-org-rifle-after-command-hook 'helm-posframe-cleanup)
-  :custom
-  (helm-posframe-height 15)
-  (helm-posframe-width (round (* (frame-width) 1.15)))
-  (helm-posframe-parameters '((left-fringe . 0)
-                              (right-fringe . 0)
-                              (internal-border-width . 12)))
-  :config
-  (setq helm-posframe-poshandler 'posframe-poshandler-frame-center))
-
-;;; Ivy Posframe
-(use-package ivy-posframe
-  :disabled
-  :if (and (window-system) (version<= "26.1" emacs-version))
-  :hook
-  (ivy-mode . ivy-posframe-mode)
-  :custom
-  (ivy-posframe-size-function 'cpm/ivy-posframe-size)
-  (ivy-posframe-height 50)
-  (ivy-posframe-width 70)
-  (ivy-posframe-parameters '((left-fringe . 0)
-                             (right-fringe . 0)
-                             (internal-border-width . 15)))
-  (ivy-posframe-display-functions-alist
-   '((swiper          . ivy-posframe-display-at-frame-top-center)
-     (swiper-isearch  . ivy-posframe-display-at-frame-top-center)
-     (complete-symbol . ivy-posframe-display-at-point)
-     (counsel-M-x     . ivy-posframe-display-at-frame-center)
-     (t               . ivy-posframe-display-at-frame-center)))
-  :custom-face
-  (ivy-posframe-cursor ((t (:background "#268bd2"))))
-  :config
-  (ivy-posframe-mode))
-
-(defun cpm/ivy-posframe-size ()
-  (list
-   :min-height ivy-height
-   :min-width (round (* (frame-width) 0.52))))
-
 ;;; Company Posframe
-;; (eval-when-compile
-;;   (quelpa
-;;    '(company-posframe :fetcher github :repo "tumashu/company-posframe")))
 (use-package company-posframe
-  :disabled
   :if (and (window-system) (version<= "26.1" emacs-version))
   :hook (company-mode . company-posframe-mode))
-
-
-;;; Hydra Posframe
-;; doesn't work well with ivy hydras
-;; (eval-when-compile
-;;   (quelpa
-;;    '(hydra-posframe :fetcher github :repo "Ladicle/hydra-posframe")))
-(use-package hydra-posframe
-  :disabled
-  :if (and (window-system) (version<= "26.1" emacs-version))
-  :hook (after-init . hydra-posframe-enable)
-  :config
-  (setq hydra-posframe-border-width 15))
 
 ;;; End posframe
 (provide 'setup-childframe)
