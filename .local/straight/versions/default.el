6f3e1cc2da4a43daf7dd3e9ff8aef9f57a91c5ba
Colin McLear <mclear@fastmail.com>
2021-05-29 00:12:50 -0400

    Update packages

--- .local/straight/versions/default.el
+++ .local/straight/versions/default.el
@@ -2,7 +2,7 @@
  ("benchmark-init-el" . "7a0f263282bbc86b01b662636306f22813082647")
  ("el-get" . "d76ac84ae9670de7bf7725b362cafe86688771f9")
  ("emacsmirror-mirror" . "71c9ae8e353f18f5ff6039b2e78f7ffad24fd4e1")
- ("gnu-elpa-mirror" . "3f69bd0d6dcc9060acfb4b9bd2456b360eb2fe51")
+ ("gnu-elpa-mirror" . "179a638b9374c3d2a790f030f127575da172c5b0")
  ("melpa" . "7e9db27ec78a96e067b87bb49bbe8e37fc43a3b5")
  ("outline-minor-faces" . "bb6db842e4fc2ed4d635001938ebafe93925f48c")
  ("packed" . "ed63b4803899c3719aeeba461e249c473e5b26f0")
