bbcbf126e5ca1b90e86eda3167f0ea4c2c2468fe
Colin McLear <mclear@fastmail.com>
2021-06-08 00:07:10 -0400

    Move stuff to archive

--- setup-config/setup-archive.el
+++ setup-config/setup-archive.el
@@ -1,7 +1,7 @@
 ;;;; Icomplete
 (use-package icomplete-vertical
   :disabled
-  :straight (icomplete-vertical :type git :host github :repo "oantolin/icomplete-vertical")
+  :straight (:type built-in)
   :hook (after-init . icomplete-vertical-mode)
   :general
   (:keymaps 'icomplete-minibuffer-map
@@ -25,46 +25,46 @@
   (icomplete-mode)
   (icomplete-vertical-mode))
 
-;;;; Vertico
-(use-package vertico
-  :general
-  (:keymaps 'vertico-map
-   "C-j"    'vertico-next
-   "C-k"    'vertico-previous)
-  :init
-  (vertico-mode)
-  :config
-  ;; Optionally enable cycling for `vertico-next' and `vertico-previous'.
-  (setq vertico-cycle t))
+;; ;;;; Vertico
+;; (use-package vertico
+;;   :general
+;;   (:keymaps 'vertico-map
+;;    "C-j"    'vertico-next
+;;    "C-k"    'vertico-previous)
+;;   :init
+;;   (vertico-mode)
+;;   :config
+;;   ;; Optionally enable cycling for `vertico-next' and `vertico-previous'.
+;;   (setq vertico-cycle t))
 
-;; A few more useful configurations...
-(use-package emacs
-  :init
-  ;; Add prompt indicator to `completing-read-multiple'.
-  (defun crm-indicator (args)
-    (cons (concat "[CRM] " (car args)) (cdr args)))
-  (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
+;; ;; A few more useful configurations...
+;; (use-package emacs
+;;   :init
+;;   ;; Add prompt indicator to `completing-read-multiple'.
+;;   (defun crm-indicator (args)
+;;     (cons (concat "[CRM] " (car args)) (cdr args)))
+;;   (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
 
-  ;; Grow and shrink minibuffer
-  ;;(setq resize-mini-windows t)
+;;   ;; Grow and shrink minibuffer
+;;   ;;(setq resize-mini-windows t)
 
-  ;; Do not allow the cursor in the minibuffer prompt
-  (setq minibuffer-prompt-properties
-        '(read-only t cursor-intangible t face minibuffer-prompt))
-  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
+;;   ;; Do not allow the cursor in the minibuffer prompt
+;;   (setq minibuffer-prompt-properties
+;;         '(read-only t cursor-intangible t face minibuffer-prompt))
+;;   (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
 
-  ;; Enable recursive minibuffers
-  (setq enable-recursive-minibuffers t))
+;;   ;; Enable recursive minibuffers
+;;   (setq enable-recursive-minibuffers t))
 
-;; Persist history over Emacs restarts using savehist (see setup-settings). Vertico sorts by history position.
+;; ;; Persist history over Emacs restarts using savehist (see setup-settings). Vertico sorts by history position.
 
-(use-package orderless
-  :init
-  (setq completion-styles '(orderless)
-        completion-category-defaults nil
-        completion-category-overrides '((file (styles . (partial-completion))))))
+;; (use-package orderless
+;;   :init
+;;   (setq completion-styles '(orderless)
+;;         completion-category-defaults nil
+;;         completion-category-overrides '((file (styles . (partial-completion))))))
 
-(selectrum-mode -1)
+;; (selectrum-mode -1)
 
 ;;;; Sticky Buffer
 ;; Stick/Lock buffer to window, courtesy of ShingoFukuyama.
