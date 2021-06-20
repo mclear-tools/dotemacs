d7a643a34a31aa9ade5f5678e3a8cea50a42d3ab
Colin McLear <mclear@fastmail.com>
2021-06-19 22:39:04 -0400

    Revise completion settings
    - Add selectrum info functions
    - Load from selectrum repo not melpa
    - Fix selectrum load
    - Don't use preview on all consult commands
    - Add function for narrowing consult commands
    - Add consult-flycheck

--- setup-config/setup-completion.el
+++ setup-config/setup-completion.el
@@ -45,7 +45,7 @@
 ;;;; Selectrum
 ;; Good completion package -- much more sane and organized than ivy. Still more than I really need but works well
 (use-package selectrum
-  :straight t
+  :straight (:host github :repo "raxod502/selectrum")
   :hook (after-init . selectrum-mode)
   :general
   (:keymaps 'selectrum-minibuffer-map
@@ -62,13 +62,10 @@
   (setq selectrum-extend-current-candidate-highlight t)
   (setq selectrum-count-style 'current/matches)
   (setq selectrum-highlight-candidates-function #'orderless-highlight-matches)
-  (setq selectrum-refine-candidates-function #'orderless-filter)
-  (selectrum-mode +1)
-  )
+  (setq selectrum-refine-candidates-function #'orderless-filter))
 
 ;; history
 (use-package selectrum-prescient
-  :disabled
   :straight t
   :after selectrum
   :config
@@ -77,7 +74,200 @@
   (prescient-persist-mode)
   (selectrum-prescient-mode +1))
 
-;; ordering
+;; Selectrum info search commands
+;; https://github.com/raxod502/selectrum/wiki/Useful-Commands#info
+(defvar Info-directory-list)
+(defvar Info-additional-directory-list)
+(defvar Info-default-directory-list)
+(declare-function info-initialize "info")
+(declare-function cl-mapcar "cl-lib")
+
+(defvar selectrum-info-history nil
+  "Completion history for `selectrum-info' and derived commands.")
+
+(defun selectrum--info-section-candidates (top-node)
+  "Return an alist of sections and candidates in the Info buffer TOP-NODE.
+
+Candidates are returned in the order that their links are listed
+in the Info buffer, which might be different from how the
+sections are actually ordered."
+  (let ((sub-topic-format
+         ;; Node links look like "* Some Thing:: Description" or
+         ;; "* Some Thing: actual link. Description", where descriptions
+         ;; are optional and might continue on the next line.
+         ;;
+         ;; The `info' library states:
+         ;; Note that nowadays we expect Info files to be made using makeinfo.
+         ;; In particular we make these assumptions:
+         ;;  - a menu item MAY contain colons but not colon-space ": "
+         ;;  - a menu item ending with ": " (but not ":: ") is an index entry
+         ;;  - a node name MAY NOT contain a colon
+         ;; This distinction is to support indexing of computer programming
+         ;; language terms that may contain ":" but not ": ".
+         (rx "* " (group (+? (not ?:))) ":"
+             (or ":" (seq " "  (group (+? (not "."))) "."))
+             ;; Include the description, if one exists.
+             ;; If it doesn't, the line ends immediately.
+             (or "\n"
+                 (seq (0+ blank)
+                      (group (+? anychar))
+                      ;; Sometimes a heading follows on the next line,
+                      ;; and sometimes there's any empty blank line
+                      ;; (such as before a section title).  For now,
+                      ;; assume continuation lines use indentation and
+                      ;; other lines don't.
+                      "\n" (not blank))))))
+    (save-match-data
+      (save-selected-window
+        (with-temp-buffer
+          ;; Some nodes created from multiple files, so we need to create a
+          ;; buffer to make sure that we see everything.
+          (info top-node (current-buffer))
+          (goto-char (point-min))
+          (let ((candidates-alist))
+            (while (re-search-forward sub-topic-format nil t)
+              (forward-line 0)         ; Go back to start of line.
+              (let* ((node-display-name (match-string 1))
+                     (node-actual-name (or (match-string 2) node-display-name)))
+                (push (cons (concat node-display-name
+                                    (if-let ((node-description (match-string 3)))
+                                        (propertize
+                                         (thread-last node-description
+                                           (replace-regexp-in-string "\n" "")
+                                           (replace-regexp-in-string " +" " ")
+                                           (concat " - "))
+                                         'face 'completions-annotations)))
+                            node-actual-name)
+                      candidates-alist)))
+            (nreverse candidates-alist)))))))
+
+(defun selectrum--info-top-dir-menu-items ()
+  (let ((sub-topic-format
+         ;; The `info' library states:
+         ;; Note that nowadays we expect Info files to be made using makeinfo.
+         ;; In particular we make these assumptions:
+         ;;  - a menu item MAY contain colons but not colon-space ": "
+         ;;  - a menu item ending with ": " (but not ":: ") is an index entry
+         ;;  - a node name MAY NOT contain a colon
+         ;; This distinction is to support indexing of computer programming
+         ;; language terms that may contain ":" but not ": ".
+         (rx (seq "* " (group (+? anything))
+                  ": "
+                  (group "(" (+? anything) ")" (*? (not ".")))
+                  "."
+                  (zero-or-one (seq (any "\n" " " "\t")
+                                    (group (+? anychar))))
+                  "\n" (or "\n" "*")))))
+    (let ((candidates-alist))
+      ;; Go through nodes in Info buffer "(dir)Top".
+      (save-match-data
+        (save-selected-window
+          (with-temp-buffer
+            ;; Some nodes created from multiple files, so we need to create a
+            ;; buffer to make sure that we see everything.
+            (info "(dir)Top" (current-buffer))
+            (goto-char (point-min))
+            (search-forward "Menu:\n")
+            (while (re-search-forward sub-topic-format nil t)
+              (forward-line 0)          ; Go back to start of line.
+              (let* ((node-display-name (match-string-no-properties 1))
+                     (node-actual-name (or (match-string-no-properties 2) node-display-name)))
+                (push (cons (concat node-display-name
+                                    (if-let ((node-description (match-string-no-properties 3)))
+                                        (propertize
+                                         (thread-last node-description
+                                           (replace-regexp-in-string "\n" "")
+                                           (replace-regexp-in-string " +" " ")
+                                           (concat " - "))
+                                         'face 'completions-annotations)))
+                            node-actual-name)
+                      candidates-alist))))))
+      ;; In case something isn't listed (Emacs might just insert itself?), also
+      ;; add in files from the Info directories as nodes themselves.
+      (dolist (file (save-match-data
+                      (thread-last (append (or Info-directory-list
+                                               Info-default-directory-list)
+                                           Info-additional-directory-list)
+                        (mapcan (lambda (directory)
+                                  (when (file-directory-p directory)
+                                    (directory-files directory nil "\\.info" t))))
+                        (mapcar (lambda (file)
+                                  (string-match "\\(.+?\\)\\." file)
+                                  (match-string 1 file)))
+                        seq-uniq)))
+        ;; TODO: Node should actually come from opening the file.
+        (let ((node (concat "(" file ")")))
+          (unless (rassoc node candidates-alist)
+            (push (cons file node) candidates-alist))))
+      (nreverse candidates-alist))))
+
+;;;###autoload
+(defun selectrum-info (&optional top-node)
+  "Use `completing-read' to jump to an Info topic.
+
+Select from the available Info top-level nodes, then one of the sub-nodes.
+If TOP-NODE is provided, then just select from its sub-nodes."
+  (interactive)
+  (unless top-node
+    (setq top-node
+          (let* ((items (selectrum--info-top-dir-menu-items))
+                 (key (completing-read "Info node: "
+                                       (lambda (input predicate action)
+                                         (if (eq action 'metadata)
+                                             `(metadata
+                                               ;; (display-sort-function . identity)
+                                               (category              . info))
+                                           (complete-with-action action
+                                                                 items
+                                                                 input
+                                                                 predicate)))
+                                       nil
+                                       t)))
+            (cdr (assoc key items)))))
+  ;; If looking at a base node (e.g., "(emacs)"), then select from list of
+  ;; optional sub-nodes.  If looking at a normal node (e.g., "(emacs)Intro"),
+  ;; then just go there instead of asking for more sub-nodes.
+  (if (string-match-p "(.*?)\\'" top-node)
+      (let* ((section-candidates-alist (selectrum--info-section-candidates top-node))
+             (section (completing-read "Info section: "
+                                       (lambda (input predicate action)
+                                         (if (eq action 'metadata)
+                                             `(metadata
+                                               (display-sort-function . identity)
+                                               (category              . info))
+                                           (complete-with-action action
+                                                                 section-candidates-alist
+                                                                 input
+                                                                 predicate)))
+                                       nil
+                                       t nil 'selectrum-info-history)))
+        (info (concat
+               top-node
+               (cdr (assoc section section-candidates-alist)))))
+    (info top-node)))
+
+;;;###autoload
+(defun selectrum-info-elisp-manual ()
+  "Like ‘selectrum-info’, but choose nodes from the Elisp reference manual. "
+  (interactive)
+  (selectrum-info "(elisp)"))
+
+;;;###autoload
+(defun selectrum-info-emacs-manual ()
+  "Like ‘selectrum-info’, but directly choose nodes from the Emacs manual."
+  (interactive)
+  (selectrum-info "(emacs)"))
+
+;;;###autoload
+(defun selectrum-info-org-manual ()
+  "Like ‘selectrum-info’, but directly choose nodes from the Org manual."
+  (interactive)
+  (selectrum-info "(org)"))
+
+;; Bind keys for selectrum-info
+(general-define-key "C-h i" #'selectrum-info)
+
+;;;; Ordering
 (use-package orderless
   :straight t
   :after selectrum
@@ -173,12 +363,19 @@
   ;; Replace `multi-occur' with `consult-multi-occur', which is a drop-in replacement.
   (fset 'multi-occur #'consult-multi-occur)
   :config
+  ;; disable preview for certain commands
+  (consult-customize
+   consult-ripgrep consult-git-grep consult-grep
+   consult-bookmark consult-recent-file consult-xref
+   consult--source-file consult--source-project-file consult--source-bookmark
+   :preview-key (kbd "M-."))
+  (consult-customize consult-theme :preview-key '(:debounce 0.5 any))
+  ;; Make consult locate work with macos
   (setq consult-locate-command "mdfind -name ARG OPTS")
   ;; Optionally configure a function which returns the project root directory
   (autoload 'projectile-project-root "projectile")
   (setq consult-project-root-function #'projectile-project-root)
-  (setq consult-async-min-input 0)
-  (setq consult-async-input-debounce 0.01))
+  (setq consult-async-min-input 0))
 
 (use-package consult-projectile
   :straight (consult-projectile :type git :host gitlab
@@ -190,29 +387,25 @@
   (interactive)
   (with-persp-buffer-list () (consult-buffer)))
 
+;; Configure initial narrowing per command
+(defvar consult-initial-narrow-config
+  '((consult-projectile . ?p)))
+
+;; Add initial narrowing hook
+(defun consult-initial-narrow ()
+  (when-let (key (alist-get this-command consult-initial-narrow-config))
+    (setq unread-command-events (append unread-command-events (list key 32)))))
+;; (add-hook 'minibuffer-setup-hook #'consult-initial-narrow)
+
 ;; Search at point with consult
 (defun consult-line-symbol-at-point ()
   (interactive)
   (consult-line (thing-at-point 'symbol)))
 
-;; Optionally configure narrowing key.
-;; Both < and C-+ work reasonably well.
-;; (setq consult-narrow-key "<") ;; (kbd "C-+")
-
-;; Optionally make narrowing help available in the minibuffer.
-;; Probably not needed if you are using which-key.
-;; (define-key consult-narrow-map (vconcat consult-narrow-key "?") #'consult-narrow-help)
-
-;; Optional configure a view library to be used by `consult-buffer'.
-;; The view library must provide two functions, one to open the view by name,
-;; and one function which must return a list of views as strings.
-;; Example: https://github.com/minad/bookmark-view/
-;; (setq consult-view-open-function #'bookmark-jump
-;;       consult-view-list-function #'bookmark-view-names)
-
-;; Optionally enable previews. Note that individual previews can be disabled
-;; via customization variables.
-;; (consult-preview-mode)
+(use-package consult-flycheck
+  :straight (:host github :repo "minad/consult-flycheck")
+  :after flycheck
+  :commands (consult-flycheck consult-flymake))
 
 ;;; In-Buffer Completion
 ;;;; Company
