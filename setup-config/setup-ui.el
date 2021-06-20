ef7b7c1bb8b3a9c7a7fa25b82239d9384fb6acdc
Colin McLear <mclear@fastmail.com>
2021-06-20 00:24:10 -0400

    Revise frame settings
    - Add frame centering function
    - Adjust height & width of default frames

--- setup-config/setup-ui.el
+++ setup-config/setup-ui.el
@@ -38,6 +38,8 @@
   (setq-default default-frame-alist
                 (append (list
 	                     '(font . "SF Mono:style=medium:size=15")
+                         '(height . 45)
+                         '(width . 85)
                          '(internal-border-width . 20)
                          '(left-fringe    . 0)
                          '(right-fringe   . 0)
@@ -80,6 +82,27 @@
   :commands ns-auto-titlebar-mode
   :if (eq system-type 'darwin)
   :init (ns-auto-titlebar-mode))
+
+;;;; Center Frames
+;; https://christiantietze.de/posts/2021/06/emacs-center-window-single-function/
+(defun cpm/frame-recenter (&optional frame)
+  "Center FRAME on the screen.
+FRAME can be a frame name, a terminal name, or a frame.
+If FRAME is omitted or nil, use currently selected frame."
+  (interactive)
+  (unless (eq 'maximised (frame-parameter nil 'fullscreen))
+    (let* ((frame (or (and (boundp 'frame) frame) (selected-frame)))
+           (frame-w (frame-pixel-width frame))
+           (frame-h (frame-pixel-height frame))
+           ;; frame-monitor-workarea returns (x y width height) for the monitor
+           (monitor-w (nth 2 (frame-monitor-workarea frame)))
+           (monitor-h (nth 3 (frame-monitor-workarea frame)))
+           (center (list (/ (- monitor-w frame-w) 2)
+                         (/ (- monitor-h frame-h) 2))))
+      (apply 'set-frame-position (flatten-list (list frame center))))))
+
+(add-hook 'after-init-hook #'cpm/frame-recenter)
+(add-hook 'after-make-frame-functions #'cpm/frame-recenter)
 
 ;;; Color
 (setq-default ns-use-srgb-colorspace t)
