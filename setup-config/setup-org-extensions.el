ab963c1ab0b737c21f14f549309a7b57bdd67684
Colin McLear <mclear@fastmail.com>
2021-06-19 22:40:56 -0400

    Revise org icons settings

--- setup-config/setup-org-extensions.el
+++ setup-config/setup-org-extensions.el
@@ -84,7 +84,7 @@ Instead it's simpler to use bash."
 (use-package org-superstar
   :hook (org-mode . org-superstar-mode)
   :config
-  (setq org-superstar-headline-bullets-list '("●" "◉" "⁂" "❖" "✮" "✱" "⚫" "✸"))
+  (setq org-superstar-headline-bullets-list '("●" "◉" "⁂" "❖" "🟊" "🞷" "🞻" "✸"))
   (setq org-superstar-prettify-item-bullets t)
   ;; see https://unicode-table.com/en/25C9/ for ideas
   (setq org-superstar-item-bullet-alist
@@ -103,86 +103,35 @@ Instead it's simpler to use bash."
 (setq org-list-indent-offset 1)
 
 ;;; Org Prettify Source Blocks
-;; Make source blocks look better. Courtesy of
-;; [[https://pank.eu/blog/pretty-babel-src-blocks.html][Rasmus Pank Roulund]]. Last
-;; updated: 2018-04-06
+;; See discussion in https://www.reddit.com/r/emacs/comments/o04it0/share_your_prettifysymbolsalist/
+(defun cpm/org-icons ()
+  (interactive)
+  "Beautify org mode keywords."
+  (setq prettify-symbols-alist
+        (mapcan (lambda (x) (list x (cons (upcase (car x)) (cdr x))))
+                '(("#+header:" . "☰")
+                  ("#+begin_src" . "╦")
+                  ("#+end_src"   . "╩")
+                  ("#+begin_comment" . "✎")
+                  ("#+end_comment" . "✎")
+                  ("#+begin_notes" . "➤")
+                  ("#+end_notes" . "➤")
+                  ("#+begin_quote" . "»")
+                  ("#+end_quote" . "«")
+                  ("[ ]" . "")
+                  ("[X]" . "")
+                  ("[-]" . "")
+                  (":PROPERTIES:" . "")
+                  (":END:" . "―")
+                  ("#+STARTUP:" . "")
+                  ("#+ROAM_TAGS:" . "")
+                  ("#+FILETAGS:" . "")
+                  ("SCHEDULED:" . "")
+                  ("DEADLINE:" . "")
+                  (":logbook:" . ""))))
+  (prettify-symbols-mode 1))
+(add-hook 'org-mode-hook #'cpm/org-icons)
 
-(with-eval-after-load 'org
-  (defvar-local rasmus/org-at-src-begin -1
-    "Variable that holds whether last position was a ")
-
-  (defvar rasmus/ob-header-symbol ?☰
-    "Symbol used for babel headers")
-
-  (defun rasmus/org-prettify-src--update ()
-    (let ((case-fold-search t)
-          (re "^[ \t]*#\\+begin_src[ \t]+[^ \f\t\n\r\v]+[ \t]*")
-          found)
-      (save-excursion
-        (goto-char (point-min))
-        (while (re-search-forward re nil t)
-          (goto-char (match-end 0))
-          (let ((args (org-trim
-                       (buffer-substring-no-properties (point)
-                                                       (line-end-position)))))
-            (when (org-string-nw-p args)
-              (let ((new-cell (cons args rasmus/ob-header-symbol)))
-                (cl-pushnew new-cell prettify-symbols-alist :test #'equal)
-                (cl-pushnew new-cell found :test #'equal)))))
-        (setq prettify-symbols-alist
-              (cl-set-difference prettify-symbols-alist
-                                 (cl-set-difference
-                                  (cl-remove-if-not
-                                   (lambda (elm)
-                                     (eq (cdr elm) rasmus/ob-header-symbol))
-                                   prettify-symbols-alist)
-                                  found :test #'equal)))
-        ;; Clean up old font-lock-keywords.
-        (font-lock-remove-keywords nil prettify-symbols--keywords)
-        (setq prettify-symbols--keywords (prettify-symbols--make-keywords))
-        (font-lock-add-keywords nil prettify-symbols--keywords)
-        (while (re-search-forward re nil t)
-          (font-lock-flush (line-beginning-position) (line-end-position))))))
-
-  (defun rasmus/org-prettify-src ()
-    "Hide src options via `prettify-symbols-mode'.
-
-    `prettify-symbols-mode' is used because it has uncollpasing. It's
-    may not be efficient."
-    (let* ((case-fold-search t)
-           (at-src-block (save-excursion
-                           (beginning-of-line)
-                           (looking-at "^[ \t]*#\\+begin_src[ \t]+[^ \f\t\n\r\v]+[ \t]*"))))
-      ;; Test if we moved out of a block.
-      (when (or (and rasmus/org-at-src-begin
-                     (not at-src-block))
-                ;; File was just opened.
-                (eq rasmus/org-at-src-begin -1))
-        (rasmus/org-prettify-src--update))
-      ;; Remove composition if at line; doesn't work properly.
-      ;; (when at-src-block
-      ;;   (with-silent-modifications
-      ;;     (remove-text-properties (match-end 0)
-      ;;                             (1+ (line-end-position))
-      ;;                             '(composition))))
-      (setq rasmus/org-at-src-begin at-src-block)))
-
-  (defun rasmus/org-prettify-symbols ()
-    (mapc (apply-partially 'add-to-list 'prettify-symbols-alist)
-          (cl-reduce 'append
-                     (mapcar (lambda (x) (list x (cons (downcase (car x)) (cdr x))))
-                             `(("#+begin_src" . ?╦) ;; ➤ 🖝 ➟ ➤ ✎ ✎
-                               ("#+end_src"   . ?╩) ;; □
-                               ("#+header:" . ,rasmus/ob-header-symbol)
-                               ("#+begin_comment" . ?✎)
-                               ("#+end_comment" . ?✎)
-                               ("#+begin_notes" . ?➤)
-                               ("#+end_notes" . ?➤)
-                               ("#+begin_quote" . ?»)
-                               ("#+end_quote" . ?«)))))
-    (turn-on-prettify-symbols-mode)
-    (add-hook 'post-command-hook 'rasmus/org-prettify-src t t))
-  (add-hook 'org-mode-hook #'rasmus/org-prettify-symbols))
 
 ;;; Org Show Markup/Pretty Entities
 ;; show markup at point -- this should be part of org!
