3d5bbe3dc5e04b07c0a80ee735738878bb25898c
Colin McLear <mclear@fastmail.com>
2021-07-09 11:57:43 -0400

    Revise setup of bibtex-actions

--- setup-config/setup-writing.el
+++ setup-config/setup-writing.el
@@ -93,58 +93,61 @@
              bibtex-actions-open-notes
              bibtex-actions-open-entry
              bibtex-actions-add-pdf-to-library)
+  :custom
+  (bibtex-actions-template '((t . "${author:15}   ${title:40}   ${year:4}")))
+  (bibtex-actions-template-suffix '((t . "   ${=key=:15}  ${=type=:12}    ${tags:*}")))
   :config
+  ;; use icons
+  (setq bibtex-actions-symbols
+        `((pdf . (,(all-the-icons-icon-for-file "foo.pdf" :face 'all-the-icons-dred) .
+                  ,(all-the-icons-icon-for-file "foo.pdf" :face 'bibtex-actions-icon-dim)))
+          (note . (,(all-the-icons-icon-for-file "foo.txt") .
+                   ,(all-the-icons-icon-for-file "foo.txt" :face 'bibtex-actions-icon-dim)))
+          (link .
+                (,(all-the-icons-faicon "external-link-square" :v-adjust 0.02 :face 'all-the-icons-dpurple) .
+                 ,(all-the-icons-faicon "external-link-square" :v-adjust 0.02 :face 'bibtex-actions-icon-dim)))))
+  ;; Here we define a face to dim non 'active' icons, but preserve alignment
+  (defface bibtex-actions-icon-dim
+    '((((background dark)) :inherit bespoke-highlight)
+      (((background light)) :inherit bespoke-highlight))
+    "Face for obscuring/dimming icons"
+    :group 'all-the-icons-faces)
+
+  ;; (setq bibtex-actions-symbols
+  ;;       `((pdf  "" . " ")
+  ;;         (note "" . " ")
+  ;;         (link "" . " ")))
+
+  ;; don't autopopulate initial input
+  (setq bibtex-actions-initial-inputs
+        '((pdf    . nil)
+          (note   . nil)
+          (link   . nil)
+          (source . nil)))
+
   ;; Set insert citekey with markdown citekeys for org-mode
   (setq bibtex-completion-format-citation-functions
         '((org-mode    . bibtex-completion-format-citation-pandoc-citeproc)
           (latex-mode    . bibtex-completion-format-citation-cite)
           (markdown-mode . bibtex-completion-format-citation-pandoc-citeproc)
           (default       . bibtex-completion-format-citation-default)))
-  (setq bibtex-completion-display-formats
-        '((t . "${author:16} ${title:36} ${year:4} ${=has-pdf=:1}${=has-note=:1} ${=type=:7}")))
-  ;; Set default action for ivy-bibtex to edit notes file
-  (setq bibtex-completion-pdf-symbol "⌘")
-  (setq bibtex-completion-notes-symbol "✎")
+
+  ;; Notes templates
   (setq bibtex-completion-notes-template-one-file "* ${author} (${date}): ${title} \n :PROPERTIES:\n :INTERLEAVE_PDF: ${file}\n :Custom_ID: ${=key=}\n :END:\n [[pdfview:${file}][file link]]")
   (setq bibtex-completion-notes-template-multiple-files "#+TITLE: ${author-or-editor} (${year}): ${title}\n#+ROAM_KEY: cite:${=key=}\n#+SETUPFILE: ./hugo_setup.org\n#+HUGO_SECTION: reading-notes\n\n- Tags :: \n- Bookends link :: bookends://sonnysoftware.com/${beref}\n- PDF :: [[${file}][PDF Link]]\n\n#+BEGIN_SRC bibtex\n (insert (org-ref-get-bibtex-entry \"${=key=}\"))\n#+END_SRC")
+
+  ;; Library paths
   (setq bibtex-completion-bibliography "~/Dropbox/Work/bibfile.bib"
-        bibtex-completion-library-path "~/Dropbox/Work/be-library/"
+        bibtex-completion-library-path "~/Library/Mobile Documents/iCloud~com~sonnysoftware~bot/Documents/be-library"
         bibtex-completion-pdf-field nil
         bibtex-completion-notes-path "~/Dropbox/Work/projects/notebook/content-org"
         bibtex-completion-notes-extension ".org"
         )
+
   ;; use w/embark-act
   (with-eval-after-load 'embark
-    (setf (alist-get 'bibtex embark-keymap-alist) 'bibtex-actions-map))
-  )
+    (setf (alist-get 'bibtex embark-keymap-alist) 'bibtex-actions-map)))
 
-;;; Helm Bibtex
-(use-package helm-bibtex
-  :disabled
-  :straight t
-  :commands helm-bibtex
-  :after helm
-  :config
-  ;; Set insert citekey with markdown citekeys for org-mode
-  (setq bibtex-completion-format-citation-functions
-        '((org-mode    . bibtex-completion-format-citation-pandoc-citeproc)
-          (latex-mode    . bibtex-completion-format-citation-cite)
-          (markdown-mode . bibtex-completion-format-citation-pandoc-citeproc)
-          (default       . bibtex-completion-format-citation-default)))
-  (setq bibtex-completion-display-formats
-        '((t . "${author:16} ${title:36} ${year:4} ${=has-pdf=:1}${=has-note=:1} ${=type=:7}")))
-  ;; Set default action for ivy-bibtex to edit notes file
-  (setq helm-bibtex-default-action 'helm-bibtex-edit-notes)
-  (setq bibtex-completion-pdf-symbol "⌘")
-  (setq bibtex-completion-notes-symbol "✎")
-  (setq bibtex-completion-notes-template-one-file "* ${author} (${date}): ${title} \n :PROPERTIES:\n :INTERLEAVE_PDF: ${file}\n :Custom_ID: ${=key=}\n :END:\n [[pdfview:${file}][file link]]")
-  (setq bibtex-completion-notes-template-multiple-files "#+TITLE: ${author-or-editor} (${year}): ${title}\n#+ROAM_KEY: cite:${=key=}\n#+SETUPFILE: ./hugo_setup.org\n#+HUGO_SECTION: reading-notes\n\n- Tags :: \n- Bookends link :: bookends://sonnysoftware.com/${beref}\n- PDF :: [[${file}][PDF Link]]\n\n#+BEGIN_SRC bibtex\n (insert (org-ref-get-bibtex-entry \"${=key=}\"))\n#+END_SRC")
-  (setq bibtex-completion-bibliography "~/Dropbox/Work/bibfile.bib"
-        bibtex-completion-library-path "~/Dropbox/Work/be-library/"
-        bibtex-completion-pdf-field nil
-        bibtex-completion-notes-path "~/Dropbox/Work/projects/notebook/content-org"
-        bibtex-completion-notes-extension ".org"
-        helm-bibtex-full-frame nil))
 
 ;;; Org Ref
 (use-package org-ref
