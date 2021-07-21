bf7ea73916c3c974ca7bb74fc0c0ea6965c5ecfa
Colin McLear <mclear@fastmail.com>
2021-07-20 23:44:38 -0400

    Replace miniframe with minipopup

--- setup-config/setup-childframe.el
+++ setup-config/setup-childframe.el
@@ -1,40 +1,73 @@
 ;; Open a child frame.
 
-;;; Miniframe
-;; Provides a great ui for completion, similar to posframe
-(use-package mini-frame
-  :straight (:type git :host github :repo "muffinmad/emacs-mini-frame")
-  :hook (after-init . mini-frame-mode)
-  :commands (mini-frame-mode)
-  :custom
-  (mini-frame-show-parameters
-   `((top    . 0.023)
-     (width  . 0.98)
-     (left   . 0.5)
-     (height . 11)
-     (child-frame-border-width . 15)
-     (internal-border-width . 0)
-     (left-fringe . 20)
-     (right-fringe . 20)
-     ;; set colors for bespoke theme
-     (foreground-color . ,bespoke-strong)
-     (background-color . ,bespoke-subtle)
-     ))
-  ;; (mini-frame-color-shift-step 7)
-  (mini-frame-advice-functions '(read-from-minibuffer
-                                 read-string
-                                 completing-read))
-  (mini-frame-resize nil)
+;;; Mini Popup
+;; Pop up for vertico
+(use-package mini-popup
+  :straight (:host github :repo "minad/mini-popup")
+  :if (display-graphic-p)
+  ;; ;; Need to set keybindings for vertico
+  :general
+  (:states '(normal insert motion emacs) :keymaps 'vertico-map
+   "<escape>" #'minibuffer-keyboard-quit
+   "C-n"      #'vertico-next-group
+   "C-p"      #'vertico-previous-group
+   "C-j"      #'vertico-next
+   "C-k"      #'vertico-previous
+   "M-RET"    #'vertico-exit)
+  :custom-face
+  (mini-popup-background ((t (:background ,bespoke-subtle))))
+  (mini-popup-border ((t (:background ,bespoke-subtle))))
+  :hook (after-init . mini-popup-mode)
   :config
-  (setq mini-frame-ignore-commands
-        '("edebug-eval-expression" debugger-eval-expression))
-  (setq mini-frame-resize 'not-set))
+  (setq  mini-popup--frame-parameters
+         '((no-accept-focus . t)
+           (min-width . t)
+           (min-height . t)
+           ;; (top . 0.023)
+           (top . 0.0)
+           (left . 0.5)
+           (width . .855)
+           (height . 10)
+           (border-width . 15)
+           (child-frame-border-width . 15)
+           (left-fringe . 20)
+           (right-fringe . 20)
+           (vertical-scroll-bars . nil)
+           (horizontal-scroll-bars . nil)
+           (menu-bar-lines . 0)
+           (tool-bar-lines . 0)
+           (tab-bar-lines . 0)
+           (no-other-frame . t)
+           (unsplittable . t)
+           (undecorated . t)
+           (cursor-type . t)
+           (minibuffer . nil)
+           (visibility . nil)
+           (no-special-glyphs . t)
+           (desktop-dont-save . t)))
+  ;; Configure a height function (Example for Vertico)
+  (defun mini-popup-height-resize ()
+    (* (1+ (min vertico--total vertico-count)) (default-line-height)))
+  (defun mini-popup-height-fixed ()
+    (* (1+ (if vertico--input vertico-count 0)) (default-line-height)))
+  (setq mini-popup--height-function #'mini-popup-height-fixed)
 
+  ;; Disable the minibuffer resizing of Vertico (HACK)
+  (advice-add #'vertico--resize-window :around
+              (lambda (&rest args)
+                (unless mini-popup-mode
+                  (apply args))))
+
+  ;; Ensure that the popup is updated after refresh (Consult-specific)
+  (add-hook 'consult--completion-refresh-hook
+            (lambda (&rest _) (mini-popup--setup)) 99))
+
+;;; Reload Child-Frame
 (defun cpm/reload-child-frame ()
   "function to reload child-frame settings"
   (interactive)
   (load-library "setup-childframe"))
-;; add hook to reload mini-frame colors on bespoke-theme change
+;; add hook to reload child-frame colors on bespoke-theme change
 (add-hook 'after-load-theme-hook 'cpm/reload-child-frame)
 
 ;;; Posframe
