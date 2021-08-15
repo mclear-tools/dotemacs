1a5d5b78eb0a2fbad73352c741a9fa0ece29b433
Colin McLear <mclear@fastmail.com>
2021-08-15 00:17:23 -0400

    Revise notes settings
    - revise loading
    - revise org-slug function
    - update autosync

--- setup-config/setup-notes.el
+++ setup-config/setup-notes.el
@@ -41,16 +41,19 @@
 (use-package org-roam
   :commands (cpm/find-note-relation
              org-roam-node-find
-             org-roam-capure
-             org-roam-setup)
-  :hook (org-mode . org-roam-setup)
+             org-roam-node-insert
+             org-roam-capture
+             org-roam-buffer-toggle)
+  ;; :hook (org-mode . org-roam-setup)
+  :custom
+  ;; Configure dirs
+  (org-roam-directory "~/Dropbox/Work/projects/notebook/content-org/")
+  (org-roam-db-location (concat org-roam-directory "org-roam.db"))
   :init
   ;; No warnings
   (setq org-roam-v2-ack t)
   :config
-  ;; Configure dirs
-  (setq org-roam-directory "~/Dropbox/Work/projects/notebook/content-org/")
-  (setq org-roam-db-location (concat org-roam-directory "org-roam.db"))
+  (org-roam-db-autosync-mode 1)
 
   ;; Add completion
   (push 'company-capf company-backends)
@@ -58,7 +61,7 @@
   ;; Org Roam Templating
   ;; see https://org-roam.readthedocs.io/en/latest/templating/
   (setq org-roam-capture-templates
-        '(("z" "Zettel" plain "%?"
+        `(("z" "Zettel" plain "%?"
            :if-new (file+head "%<%Y-%m%d-%H%M>-${slug}.org"
                               ,(concat (concat "#+SETUPFILE:" hugo-notebook-setup-file) "\n#+HUGO_SECTION: zettel\n#+HUGO_SLUG: ${slug}\n#+TITLE: ${title}\n#+DATE: %<%Y-%m%d-%H%M>"))
            :unnarrowed t)
@@ -84,45 +87,46 @@
   ;; Use dashes rather than underscores in your slugs
   ;; Need to redefine function (no customization available)
   ;; See https://github.com/org-roam/org-roam/pull/1544
-  (cl-defmethod org-roam-node-slug ((node org-roam-node))
-    "Return the slug of NODE."
-    (let ((title (org-roam-node-title node))
-          (slug-trim-chars '(;; Combining Diacritical Marks https://www.unicode.org/charts/PDF/U0300.pdf
-                             768 ; U+0300 COMBINING GRAVE ACCENT
-                             769 ; U+0301 COMBINING ACUTE ACCENT
-                             770 ; U+0302 COMBINING CIRCUMFLEX ACCENT
-                             771 ; U+0303 COMBINING TILDE
-                             772 ; U+0304 COMBINING MACRON
-                             774 ; U+0306 COMBINING BREVE
-                             775 ; U+0307 COMBINING DOT ABOVE
-                             776 ; U+0308 COMBINING DIAERESIS
-                             777 ; U+0309 COMBINING HOOK ABOVE
-                             778 ; U+030A COMBINING RING ABOVE
-                             780 ; U+030C COMBINING CARON
-                             795 ; U+031B COMBINING HORN
-                             803 ; U+0323 COMBINING DOT BELOW
-                             804 ; U+0324 COMBINING DIAERESIS BELOW
-                             805 ; U+0325 COMBINING RING BELOW
-                             807 ; U+0327 COMBINING CEDILLA
-                             813 ; U+032D COMBINING CIRCUMFLEX ACCENT BELOW
-                             814 ; U+032E COMBINING BREVE BELOW
-                             816 ; U+0330 COMBINING TILDE BELOW
-                             817 ; U+0331 COMBINING MACRON BELOW
-                             )))
-      (cl-flet* ((nonspacing-mark-p (char)
-                                    (memq char slug-trim-chars))
-                 (strip-nonspacing-marks (s)
-                                         (ucs-normalize-NFC-string
-                                          (apply #'string (seq-remove #'nonspacing-mark-p
-                                                                      (ucs-normalize-NFD-string s)))))
-                 (cl-replace (title pair)
-                             (replace-regexp-in-string (car pair) (cdr pair) title)))
-        (let* ((pairs `(("[^[:alnum:][:digit:]]" . "-")  ;; convert anything not alphanumeric
-                        ("--*" . "-")  ;; remove sequential underscores
-                        ("^-" . "")    ;; remove starting underscore
-                        ("-$" . "")))  ;; remove ending underscore
-               (slug (-reduce-from #'cl-replace (strip-nonspacing-marks title) pairs)))
-          (downcase slug)))))
+  (with-eval-after-load "org-roam"
+    (cl-defmethod org-roam-node-slug ((node org-roam-node))
+      "Return the slug of NODE."
+      (let ((title (org-roam-node-title node))
+            (slug-trim-chars '(;; Combining Diacritical Marks https://www.unicode.org/charts/PDF/U0300.pdf
+                               768 ; U+0300 COMBINING GRAVE ACCENT
+                               769 ; U+0301 COMBINING ACUTE ACCENT
+                               770 ; U+0302 COMBINING CIRCUMFLEX ACCENT
+                               771 ; U+0303 COMBINING TILDE
+                               772 ; U+0304 COMBINING MACRON
+                               774 ; U+0306 COMBINING BREVE
+                               775 ; U+0307 COMBINING DOT ABOVE
+                               776 ; U+0308 COMBINING DIAERESIS
+                               777 ; U+0309 COMBINING HOOK ABOVE
+                               778 ; U+030A COMBINING RING ABOVE
+                               780 ; U+030C COMBINING CARON
+                               795 ; U+031B COMBINING HORN
+                               803 ; U+0323 COMBINING DOT BELOW
+                               804 ; U+0324 COMBINING DIAERESIS BELOW
+                               805 ; U+0325 COMBINING RING BELOW
+                               807 ; U+0327 COMBINING CEDILLA
+                               813 ; U+032D COMBINING CIRCUMFLEX ACCENT BELOW
+                               814 ; U+032E COMBINING BREVE BELOW
+                               816 ; U+0330 COMBINING TILDE BELOW
+                               817 ; U+0331 COMBINING MACRON BELOW
+                               )))
+        (cl-flet* ((nonspacing-mark-p (char)
+                                      (memq char slug-trim-chars))
+                   (strip-nonspacing-marks (s)
+                                           (ucs-normalize-NFC-string
+                                            (apply #'string (seq-remove #'nonspacing-mark-p
+                                                                        (ucs-normalize-NFD-string s)))))
+                   (cl-replace (title pair)
+                               (replace-regexp-in-string (car pair) (cdr pair) title)))
+          (let* ((pairs `(("[^[:alnum:][:digit:]]" . "-")  ;; convert anything not alphanumeric
+                          ("--*" . "-")  ;; remove sequential underscores
+                          ("^-" . "")    ;; remove starting underscore
+                          ("-$" . "")))  ;; remove ending underscore
+                 (slug (-reduce-from #'cl-replace (strip-nonspacing-marks title) pairs)))
+            (downcase slug))))))
 
   ;; Showing the number of backlinks for each node in org-roam-node-find
   ;; https://github.com/org-roam/org-roam/wiki/Hitchhiker's-Rough-Guide-to-Org-roam-V2
@@ -143,33 +147,34 @@
 
   ;; Update when idle
   ;; https://orgmode-exocortex.com/2021/07/22/configure-org-roam-v2-to-update-database-only-when-idle/
-  (with-eval-after-load "org-roam"
-    ;; queue for files that will be updated in org-roam-db when emacs is idle
-    (setq org-roam-db-update-queue (list))
-    ;; save the original update function;
-    (setq orig-update-file (symbol-function 'org-roam-db-update-file))
-    ;; then redefine the db update function to add the filename to a queue
-    (defun org-roam-db-update-file (&optional file-path)
-      ;; do same logic as original to determine current file-path if not passed as arg
-      (setq file-path (or file-path (buffer-file-name (buffer-base-buffer))))
-      (message "org-roam: scheduling update of %s" file-path)
-      (if (not (memq file-path org-roam-db-update-queue))
-          (push file-path org-roam-db-update-queue)))
+  ;; FIXME -- seems not to be working quite right
+  ;; (with-eval-after-load "org-roam"
+  ;;   ;; queue for files that will be updated in org-roam-db when emacs is idle
+  ;;   (setq org-roam-db-update-queue (list))
+  ;;   ;; save the original update function;
+  ;;   (setq orig-update-file (symbol-function 'org-roam-db-update-file))
+  ;;   ;; then redefine the db update function to add the filename to a queue
+  ;;   (defun org-roam-db-update-file (&optional file-path)
+  ;;     ;; do same logic as original to determine current file-path if not passed as arg
+  ;;     (setq file-path (or file-path (buffer-file-name (buffer-base-buffer))))
+  ;;     (message "org-roam: scheduling update of %s" file-path)
+  ;;     (if (not (memq file-path org-roam-db-update-queue))
+  ;;         (push file-path org-roam-db-update-queue)))
 
-    ;; this function will be called when emacs is idle for a few seconds
-    (defun org-roam-db-idle-update-files ()
-      ;; go through queued filenames one-by-one and update db
-      ;; if we're not idle anymore, stop. will get rest of queue next idle.
-      (while (and org-roam-db-update-queue (current-idle-time))
-        ;; apply takes function var and list
-        (apply orig-update-file (list (pop org-roam-db-update-queue)))))
+  ;;   ;; this function will be called when emacs is idle for a few seconds
+  ;;   (defun org-roam-db-idle-update-files ()
+  ;;     ;; go through queued filenames one-by-one and update db
+  ;;     ;; if we're not idle anymore, stop. will get rest of queue next idle.
+  ;;     (while (and org-roam-db-update-queue (current-idle-time))
+  ;;       ;; apply takes function var and list
+  ;;       (apply orig-update-file (list (pop org-roam-db-update-queue)))))
 
-    ;; we'll only start updating db if we've been idle for this many seconds
-    (run-with-idle-timer 5 t #'org-roam-db-idle-update-files))
+  ;;   ;; we'll only start updating db if we've been idle for this many seconds
+  ;;   (run-with-idle-timer 5 t #'org-roam-db-idle-update-files))
   )
 
 
-;;;;; Fancy Node Icons
+;;;; Fancy Node Icons
 ;; Fancy org-roam-node-find with icons and overlays (which allow for better searching whilst keeping the icons
 ;; From https://github.com/hieutkt/.doom.d/blob/master/config.el#L690-L745 or
 ;; https://orgroam.slack.com/archives/CV20S23C0/p1626662183035800
