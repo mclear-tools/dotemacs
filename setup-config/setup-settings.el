5bddfbdec9f3ca39497667cdc8d03df0f4369f3a
Colin McLear <mclear@fastmail.com>
2021-11-11 18:46:29 -0500

    Revise UTF-8 settings

--- setup-config/setup-settings.el
+++ setup-config/setup-settings.el
@@ -64,12 +64,16 @@
 (setq-default tab-always-indent t)
 
 ;;;;; UTF 8
-(setq locale-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
 (set-default-coding-systems 'utf-8)
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
 (set-selection-coding-system 'utf-8)
-(prefer-coding-system 'utf-8)
+
+(setq-default buffer-file-coding-system 'utf-8)
+(setq coding-system-for-read 'utf-8)
+(setq coding-system-for-write 'utf-8)
+(setq locale-coding-system 'utf-8)
 (setq default-buffer-file-coding-system 'utf-8)
 (setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING))
 
