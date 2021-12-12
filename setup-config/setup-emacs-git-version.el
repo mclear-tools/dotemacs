1acc705587d98c55c0b843ee64ab07915873bd8f
Colin McLear <mclear@fastmail.com>
2021-12-12 01:20:44 -0500

    Update git version

--- setup-config/setup-emacs-git-version.el
+++ setup-config/setup-emacs-git-version.el
@@ -17,7 +17,7 @@
 
 ;;; Code:
 
-(defconst emacs-version-git-commit "4434deaee2"
+(defconst emacs-version-git-commit "4b29468dbe"
   "String giving the git sha1 from which this Emacs was built.
 
 SHA1 of git commit that was used to compile this version of
