c5acdbcab148f0fd6cd033d15a703e4b7f461e21
Colin McLear <mclear@fastmail.com>
2022-02-22 23:04:05 -0500

    Revise tab settings

--- setup-config/setup-tabs.el
+++ setup-config/setup-tabs.el
@@ -4,6 +4,7 @@
 ;; Use tab-bar for window grouping and configuration within a project (replaces eyebrowse)
 (use-package tab-bar
   :straight (:type built-in)
+  :after (project)
   :commands (tab-bar-new-tab
              tab-bar-switch-to-tab
              tab-bar-switch-to-next-tab
@@ -11,7 +12,9 @@
   :custom
   (tab-bar-new-tab-choice "*scratch*")
   (tab-bar-select-tab-modifiers '(super))
+  (tab-bar-close-tab-select 'recent)
   (tab-bar-new-tab-to 'rightmost)
+  (tab-bar-close-last-tab-choice 'tab-bar-mode-disable)
   (tab-bar-show nil)
   :config
   ;; https://protesilaos.com/codelog/2020-08-03-emacs-custom-functions-galore/
@@ -32,22 +35,22 @@ questions.  Else use completion to select the tab to switch to."
              (tab-bar-switch-to-tab
               (completing-read "Select tab: " tabs nil t))))))
 
-  (setq tab-bar-tab-name-function #'cpm/name-tab-by-project-or-default)
+  ;; (setq tab-bar-tab-name-function #'cpm/name-tab-by-project-or-default)
   (setq tab-bar-mode t))
 
-;; See https://www.rousette.org.uk/archives/using-the-tab-bar-in-emacs/
-;; I've modified it to use project instead of projectile
-(defun cpm/name-tab-by-project-or-default ()
-  "Return project name if in a project, or default tab-bar name if not.
-The default tab-bar name uses the buffer name."
-  (let ((project-name (cpm--project-name)))
-    (if (string= "-" project-name)
-        (tab-bar-tab-name-current)
-      (cpm--project-name))))
+;; ;; See https://www.rousette.org.uk/archives/using-the-tab-bar-in-emacs/
+;; ;; I've modified it to use project instead of projectile
+;; (defun cpm/name-tab-by-project-or-default ()
+;;   "Return project name if in a project, or default tab-bar name if not.
+;; The default tab-bar name uses the buffer name."
+;;   (let ((project-name (cpm--project-name)))
+;;     (if (string= "-" project-name)
+;;         (tab-bar-tab-name-current)
+;;       (cpm--project-name))))
 
-;; Get the current tab name for use in some other display
-(defun cpm--current-tab-name ()
-  (alist-get 'name (tab-bar--current-tab)))
+;; ;; Get the current tab name for use in some other display
+;; (defun cpm--current-tab-name ()
+;;   (alist-get 'name (tab-bar--current-tab)))
 
 ;;;; Tab Bar Echo
 ;; Use echo area for tab name display
@@ -63,7 +66,7 @@ The default tab-bar name uses the buffer name."
   (tab-bar-echo-area-mode 1))
 
 ;; display all tabs when idle
-(run-with-idle-timer 15 t (lambda () (message nil) (tab-bar-echo-area-display-tab-names)))
+(run-with-idle-timer 5 t (lambda () (message nil) (tab-bar-echo-area-display-tab-names)))
 
 ;;; Provide Tabs
 (provide 'setup-tabs)
