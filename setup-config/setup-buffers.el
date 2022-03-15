2ded44bc741a5b9f4a1d490282010b689cc101b6
Colin McLear <mclear@fastmail.com>
2022-03-15 01:28:19 -0400

    Move windows & buffer settings to separate files

--- setup-config/setup-buffers.el
+++ setup-config/setup-buffers.el
@@ -1,74 +1,34 @@
-;; Windows (and Buffers...)
+;;; setup-buffers.el --- summary -*- lexical-binding: t -*-
 
-;;; Windows
-;; Vertical window divider
-(use-package frame
-  :straight (:type built-in)
-  :custom
-  (window-divider-default-right-width 10)
-  (window-divider-default-bottom-width 1)
-  (window-divider-default-places 'right-only)
-  (window-divider-mode t))
-;; Make sure new frames use window-divider
-(add-hook 'before-make-frame-hook 'window-divider-mode)
+;; Author: Colin McLear
+;; Maintainer: Colin McLear
 
-;;; Window Movement
+;; This file is not part of GNU Emacs
 
-;; Quickly switch windows in Emacs
-(use-package ace-window
-  :commands (ace-window ace-swap-window aw-flip-window cpm/window-exchange))
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
 
-(defun cpm/other-window ()
-  (interactive)
-  (other-window 1))
-(bind-key* "C-c C-o" 'cpm/other-window)
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
 
-(use-package emacs-winum
-  :straight (winum :type git :host github :repo "deb0ch/emacs-winum")
-  :hook (after-init . winum-mode)
-  :custom
-  ;; seems to require being set in custom to take effect
-  (winum-auto-setup-mode-line nil)
-  :config
-  (setq window-numbering-scope            'global
-        winum-reverse-frame-list          nil
-        winum-auto-assign-0-to-minibuffer t
-        ;; winum-format                      " %s
-        ;; winum-mode-line-position          1
-        winum-ignored-buffers             '(" *which-key*")
-        winum-ignored-buffers-regexp      '(" \\*Treemacs-.*")))
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-(use-package windmove
-  :commands (windmove-up windmove-down windmove-left windmove-right)
-  :bind (("H-l" . #'windmove-right)
-         ("H-j" . #'windmove-down)
-         ("H-h" . #'windmove-left)
-         ("H-k" . #'windmove-up))
-  :config
-  (windmove-default-keybindings))
-(defun cpm/split-window-right-and-focus ()
-  "Split the window horizontally and focus the new window."
-  (interactive)
-  (require 'windmove)
-  (split-window-right)
-  (windmove-right))
-(defun cpm/split-window-below-and-focus ()
-  "Split the window vertically and focus the new window."
-  (interactive)
-  (require 'windmove)
-  (split-window-below)
-  (windmove-down))
 
-;;; Window Restore
-;; Winner mode is a built-in package for restoring window configurations
-;; https://www.emacswiki.org/emacs/WinnerMode
-(use-package winner
-  :straight nil
-  :hook (after-init . winner-mode))
+;;; Commentary:
 
-;;; Windows & Buffers
+;; Buffer settings & useful functions
+
+;;; Code:
+
+;;; Switch to Buffer Preserve Window
+
 ;; switch-to-buffer tries to preserve window-point
-(setq switch-to-buffer-preserve-window-point 'already-displayed)
+(setq switch-to-buffer-preserve-window-point t)
 
 ;;; Unique buffers
 (use-package uniquify
@@ -89,6 +49,18 @@
                              (let ((buffer-file-name (buffer-name)))
                                (set-auto-mode)))))
 
+;;; Autorevert
+(use-package autorevert
+  :straight (:type built-in)
+  :hook (after-init . global-auto-revert-mode)
+  :init
+  (setq auto-revert-interval .5)
+  :config
+  (setq auto-revert-verbose nil ; Shut up, please!
+        revert-without-query '(".*") ;; disable revert query
+        ;; Revert Dired buffers, too
+        global-auto-revert-non-file-buffers t)
+  (global-auto-revert-mode))
 ;;; Revert All Buffers
 
 (use-package revert-buffer-all
@@ -176,105 +148,6 @@
               (ibuffer-vc-set-filter-groups-by-vc-root)
               (unless (eq ibuffer-sorting-mode 'alphabetic)
                 (ibuffer-do-sort-by-alphabetic)))))
-
-
-;;; Custom Functions
-;;;; Toggle Dedicated Window
-(defun cpm/toggle-window-dedicated ()
-  "Toggle whether the current active window is dedicated or not"
-  (interactive)
-  (message
-   (if (let (window (get-buffer-window (current-buffer)))
-         (set-window-dedicated-p window (not (window-dedicated-p window))))
-       "Window '%s' is dedicated"
-     "Window '%s' is normal")
-   (current-buffer)))
-
-;;;; Exchange Windows
-;; Swap buffers in windows and leave the cursor in the original window. Courtesy of
-;; Mike Zamansky's video.
-;; http://cestlaz.github.io/posts/using-emacs-36-touch-of-elisp/#.WX5Wg0czpcx
-
-(defun cpm/window-exchange-buffer ()
-  "Swap buffer in windows and leave focus in original window"
-  (interactive)
-  (ace-swap-window)
-  (aw-flip-window))
-
-;;;; Rotate Windows
-;; from magnars modified by ffevotte for dedicated windows support
-(defun cpm/rotate-windows (count)
-  "Rotate your windows.
-  Dedicated windows are left untouched. Giving a negative prefix
-  argument takes the kindows rotate backwards."
-  (interactive "p")
-  (let* ((non-dedicated-windows (cl-remove-if 'window-dedicated-p (window-list)))
-         (num-windows (length non-dedicated-windows))
-         (i 0)
-         (step (+ num-windows count)))
-    (cond ((not (> num-windows 1))
-           (message "You can't rotate a single window!"))
-          (t
-           (dotimes (counter (- num-windows 1))
-             (let* ((next-i (% (+ step i) num-windows))
-
-                    (w1 (elt non-dedicated-windows i))
-                    (w2 (elt non-dedicated-windows next-i))
-
-                    (b1 (window-buffer w1))
-                    (b2 (window-buffer w2))
-
-                    (s1 (window-start w1))
-                    (s2 (window-start w2)))
-               (set-window-buffer w1 b2)
-               (set-window-buffer w2 b1)
-               (set-window-start w1 s2)
-               (set-window-start w2 s1)
-               (setq i next-i)))))))
-
-(defun cpm/rotate-windows-backward (count)
-  "Rotate your windows backward."
-  (interactive "p")
-  (cpm/rotate-windows (* -1 count)))
-
-;;;; Split Windows
-(defun cpm/toggle-window-split ()
-  "Move from a horizontal to a vertical split and vice versa"
-  (interactive)
-  (if (= (count-windows) 2)
-      (let* ((this-win-buffer (window-buffer))
-             (next-win-buffer (window-buffer (next-window)))
-             (this-win-edges (window-edges (selected-window)))
-             (next-win-edges (window-edges (next-window)))
-             (this-win-2nd (not (and (<= (car this-win-edges)
-                                         (car next-win-edges))
-                                     (<= (cadr this-win-edges)
-                                         (cadr next-win-edges)))))
-             (splitter
-              (if (= (car this-win-edges)
-                     (car (window-edges (next-window))))
-                  'split-window-horizontally
-                'split-window-vertically)))
-        (delete-other-windows)
-        (let ((first-win (selected-window)))
-          (funcall splitter)
-          (if this-win-2nd (other-window 1))
-          (set-window-buffer (selected-window) this-win-buffer)
-          (set-window-buffer (next-window) next-win-buffer)
-          (select-window first-win)
-          (if this-win-2nd (other-window 1))))))
-
-
-;;;; Jump to Minibuffer Window
-(defun cpm/goto-minibuffer-window ()
-  "locate point to minibuffer window if it is active."
-  (interactive)
-  (if (active-minibuffer-window)
-      (select-window (active-minibuffer-window))
-    (error "Minibuffer is not active")))
-
-;; (with-eval-after-load 'general
-;; (general-def "C-c m" #'cpm/goto-minibuffer-window))
 
 ;;;; Blank Buffer New Frame
 ;; Make a blank buffer when opening a new frame. From
@@ -432,6 +305,5 @@ will be killed."
   (interactive)
   (switch-to-buffer (other-buffer (current-buffer) 1)))
 
-;;; End Windows & Buffers
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(provide 'setup-windows-buffers)
+(provide 'setup-buffers)
+;;; setup-buffers.el ends here
